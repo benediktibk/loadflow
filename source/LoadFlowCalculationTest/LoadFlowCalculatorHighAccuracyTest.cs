84a591dc091bdca9e6e51b3d0aef45b37fa41709
Benedikt Schmidt <benediktibk@aon.at>
2014-02-18 15:31:23 +0100

    added test with higher accuracy

--- source/LoadFlowCalculationTest/LoadFlowCalculatorHighAccuracyTest.cs
+++ source/LoadFlowCalculationTest/LoadFlowCalculatorHighAccuracyTest.cs
@@ -10,7 +10,7 @@ namespace LoadFlowCalculationTest
         LoadFlowCalculatorSmallAccuracyTest
     {
         [TestMethod]
-        public void CalculateNodeVoltagesAndPowers_fiveNodeProblemAndVoltagesAndPowersGivenVersion2_correctResults()
+        public void CalculateNodeVoltagesAndPowers_fiveNodeProblemAndVoltagesAndPowersGivenVersionTwo_correctResults()
         {
             Matrix<Complex> admittances;
             Vector<Complex> voltages;
@@ -30,7 +30,7 @@ namespace LoadFlowCalculationTest
         }
 
         [TestMethod]
-        public void CalculateNodeVoltagesAndPowers_fiveNodeProblemAndOnlyOneVoltageGiven_correctResults()
+        public void CalculateNodeVoltagesAndPowers_fiveNodeProblemAndOnlyOneVoltageGivenVersionOne_correctResults()
         {
             Matrix<Complex> admittances;
             Vector<Complex> voltages;
@@ -43,6 +43,26 @@ namespace LoadFlowCalculationTest
             nodes[2].Voltage = voltages.At(2);
             nodes[3].Power = powers.At(3);
             nodes[4].Power = powers.At(4);
+
+            nodes = _calculator.CalculateNodeVoltagesAndPowers(admittances, nominalVoltage, nodes);
+
+            NodeAssert.AreEqual(nodes, voltages, powers, 0.0001, 0.01);
+        }
+
+        [TestMethod]
+        public void CalculateNodeVoltagesAndPowers_fiveNodeProblemAndOnlyOneVoltageGivenVersionTwo_correctResults()
+        {
+            Matrix<Complex> admittances;
+            Vector<Complex> voltages;
+            Vector<Complex> powers;
+            double nominalVoltage;
+            CreateFiveNodeProblem(out admittances, out voltages, out powers, out nominalVoltage);
+            var nodes = new[] { new Node(), new Node(), new Node(), new Node(), new Node() };
+            nodes[0].Power = powers.At(0);
+            nodes[1].Power = powers.At(1);
+            nodes[2].Power = powers.At(2);
+            nodes[3].Power = powers.At(3);
+            nodes[4].Voltage = voltages.At(4);
 
             nodes = _calculator.CalculateNodeVoltagesAndPowers(admittances, nominalVoltage, nodes);
 
