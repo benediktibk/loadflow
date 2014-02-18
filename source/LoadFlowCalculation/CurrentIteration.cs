aa57b2a9d4cd9b38ed53eab6b78d66040f7d4023
Benedikt Schmidt <benediktibk@aon.at>
2014-02-18 14:06:59 +0100

    implemented current iteration

--- source/LoadFlowCalculation/CurrentIteration.cs
+++ source/LoadFlowCalculation/CurrentIteration.cs
@@ -21,32 +21,32 @@ namespace LoadFlowCalculation
             Matrix<Complex> admittancesToUnknownVoltages,
             double nominalVoltage, Vector<Complex> knownVoltages, Vector<Complex> knownPowers)
         {
-            throw new NotImplementedException();
+            var nodeCount = admittancesToUnknownVoltages.RowCount;
+            var initialVoltages = new Complex[nodeCount];
 
-            //var nodeCount = admittances.RowCount;
-            //var initialVoltages = new Complex[nodeCount];
+            for (var i = 0; i < nodeCount; ++i)
+                initialVoltages[i] = new Complex(nominalVoltage, 0);
 
-            //for (var i = 0; i < nodeCount; ++i)
-            //    initialVoltages[i] = new Complex(nominalVoltage, 0);
+            Vector<Complex> voltages = new DenseVector(initialVoltages);
+            var iterations = 0;
+            var knownPowersConjugated = knownPowers.Conjugate();
+            var factorization = admittancesToUnknownVoltages.QR();
+            double voltageChange;
+            var constantCurrents = admittancesToKnownVoltages.Multiply(knownVoltages);
 
-            //Vector<Complex> voltages = new DenseVector(initialVoltages);
-            //var iterations = 0;
-            //var knownPowersConjugated = knownPowers.Conjugate();
-            //var factorization = admittances.QR();
-            //double voltageChange;
+            do
+            {
+                var loadCurrents = knownPowersConjugated.PointwiseDivide(voltages.Conjugate());
+                var currents = loadCurrents.Subtract(constantCurrents);
+                var newVoltages = factorization.Solve(currents);
+                var voltageDifference = newVoltages.Subtract(voltages);
+                var maximumVoltageDifference = voltageDifference.AbsoluteMaximum();
+                voltageChange = maximumVoltageDifference.Magnitude / nominalVoltage;
+                voltages = newVoltages;
+                ++iterations;
+            } while (iterations <= _maximumIterations && voltageChange > _terminationCriteria);
 
-            //do
-            //{
-            //    var currents = knownPowersConjugated.PointwiseDivide(voltages.Conjugate());
-            //    var newVoltages = factorization.Solve(currents);
-            //    var voltageDifference = newVoltages.Subtract(voltages);
-            //    var maximumVoltageDifference = voltageDifference.AbsoluteMaximum();
-            //    voltageChange = maximumVoltageDifference.Magnitude / nominalVoltage;
-            //    voltages = newVoltages;
-            //    ++iterations;
-            //} while (iterations <= _maximumIterations && voltageChange > _terminationCriteria);
-
-            //return voltages;
+            return voltages;
         }
     }
 }
