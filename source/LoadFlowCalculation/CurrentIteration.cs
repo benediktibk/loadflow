10bf417297c589666d660e3e413229a0fe25c779
Benedikt Schmidt <benediktibk@aon.at>
2014-02-18 13:56:51 +0100

    first try to implement the current iteration

--- source/LoadFlowCalculation/CurrentIteration.cs
+++ source/LoadFlowCalculation/CurrentIteration.cs
@@ -8,8 +8,8 @@ namespace LoadFlowCalculation
     public class CurrentIteration :
         LoadFlowCalculator
     {
-        private double _terminationCriteria;
-        private int _maximumIterations;
+        private readonly double _terminationCriteria;
+        private readonly int _maximumIterations;
 
         public CurrentIteration(double terminationCriteria, int maximumIterations)
         {
@@ -25,7 +25,30 @@ namespace LoadFlowCalculation
 
         override public Vector<Complex> CalculateUnknownVoltages(Matrix<Complex> admittances, double nominalVoltage, Vector<Complex> knownPowers)
         {
-            throw new NotImplementedException();
+            var nodeCount = admittances.RowCount;
+            var initialVoltages = new Complex[nodeCount];
+
+            for (var i = 0; i < nodeCount; ++i)
+                initialVoltages[i] = new Complex(nominalVoltage, 0);
+
+            Vector<Complex> voltages = new DenseVector(initialVoltages);
+            var iterations = 0;
+            var knownPowersConjugated = knownPowers.Conjugate();
+            var factorization = admittances.QR();
+            double voltageChange;
+
+            do
+            {
+                var currents = knownPowersConjugated.PointwiseDivide(voltages.Conjugate());
+                var newVoltages = factorization.Solve(currents);
+                var voltageDifference = newVoltages.Subtract(voltages);
+                var maximumVoltageDifference = voltageDifference.AbsoluteMaximum();
+                voltageChange = maximumVoltageDifference.Magnitude/nominalVoltage;
+                voltages = newVoltages;
+                ++iterations;
+            } while (iterations <= _maximumIterations && voltageChange > _terminationCriteria);
+
+            return voltages;
         }
     }
 }
--- source/LoadFlowCalculation/NodePotentialMethod.cs
+++ source/LoadFlowCalculation/NodePotentialMethod.cs
@@ -13,7 +13,7 @@ namespace LoadFlowCalculation
         {
             var ownCurrents = knownPowers.Divide(nominalVoltage);
             var otherCurrents = admittancesToKnownVoltages.Multiply(knownVoltages);
-            var totalCurrents = ownCurrents.Subtract(otherCurrents);
+            var totalCurrents = ownCurrents.Conjugate().Subtract(otherCurrents);
             var factorization = admittancesToUnknownVoltages.QR();
             var determinant = factorization.Determinant;
 
@@ -32,7 +32,7 @@ namespace LoadFlowCalculation
             if (determinant.Magnitude < 0.0001)
                 throw new NotFullRankException();
 
-            return factorization.Solve(currents);
+            return factorization.Solve(currents.Conjugate());
         }
     }
 }
