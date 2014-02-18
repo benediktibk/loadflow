8a6345b26e6a9a185de95183e20c612217547af4
Benedikt Schmidt <benediktibk@aon.at>
2014-02-18 17:47:04 +0100

    added check if value is NaN

--- source/LoadFlowCalculationTest/ComplexAssert.cs
+++ source/LoadFlowCalculationTest/ComplexAssert.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Numerics;
 
 namespace LoadFlowCalculationTest
@@ -7,14 +8,17 @@ namespace LoadFlowCalculationTest
     {
         public static void AreEqual(Complex expected, Complex actual, double delta)
         {
+            Assert.IsFalse(Double.IsNaN(actual.Real));
+            Assert.IsFalse(Double.IsNaN(actual.Imaginary));
+            Assert.IsFalse(Double.IsInfinity(actual.Real));
+            Assert.IsFalse(Double.IsInfinity(actual.Imaginary));
             Assert.AreEqual(expected.Real, actual.Real, delta);
             Assert.AreEqual(expected.Imaginary, actual.Imaginary, delta);
         }
 
         public static void AreEqual(double expectedReal, double expectedImaginary, Complex actual, double delta)
         {
-            Assert.AreEqual(expectedReal, actual.Real, delta);
-            Assert.AreEqual(expectedImaginary, actual.Imaginary, delta);
+            AreEqual(new Complex(expectedReal, expectedImaginary), actual, delta);
         }
     }
 }
