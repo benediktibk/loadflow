c77ae4893848511aee0730e53b340b9178239769
Benedikt Schmidt <benediktibk@aon.at>
2014-02-20 19:34:06 +0100

    added tests for CalculatorDouble

---
+++ source/AnalyticContinuationTest/CalculatorDoubleTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using AnalyticContinuation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnalyticContinuationTest
+{
+    [TestClass]
+    public class CalculatorDoubleTest
+    {
+        private CalculatorDouble _calculator;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _calculator = new CalculatorDouble();
+        }
+
+        [TestMethod]
+        public void Add_piAndE_correctResult()
+        {
+            Assert.AreEqual(Math.E + Math.PI, _calculator.Add(Math.E, Math.PI));
+        }
+
+        [TestMethod]
+        public void Subtract_piAndE_correctResult()
+        {
+            Assert.AreEqual(Math.E - Math.PI, _calculator.Subtract(Math.E, Math.PI));
+        }
+
+        [TestMethod]
+        public void Multiply_piAndE_correctResult()
+        {
+            Assert.AreEqual(Math.E * Math.PI, _calculator.Multiply(Math.E, Math.PI));
+        }
+
+        [TestMethod]
+        public void Divide_piAndE_correctResult()
+        {
+            Assert.AreEqual(Math.E / Math.PI, _calculator.Divide(Math.E, Math.PI));
+        }
+
+        [TestMethod]
+        public void AssignFromDouble_pi_correctResult()
+        {
+            Assert.AreEqual(Math.PI, _calculator.AssignFromDouble(Math.PI));
+        }
+    }
+}
