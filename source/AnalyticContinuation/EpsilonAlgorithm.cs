ebeefc75752a27d61c0b626fb156ec0789b629fc
Benedikt Schmidt <benediktibk@aon.at>
2014-02-22 20:23:45 +0100

    first implementation of the epsilon algorithm

--- source/AnalyticContinuation/EpsilonAlgorithm.cs
+++ source/AnalyticContinuation/EpsilonAlgorithm.cs
@@ -1,19 +1,52 @@
 ﻿using System;
+using System.Linq;
 
 namespace AnalyticContinuation
 {
     public class EpsilonAlgorithm<T> : IAnalyticContinuation<T> where T : struct, IEquatable<T>, IFormattable
     {
         private readonly PowerSeries<T> _powerSeries;
+        private readonly ICalculatorGeneric<T> _calculator; 
+        private T[] _epsilonPrevious;
+        private T[] _epsilonCurrent;
+        private T[] _epsilonNext;
  
         public EpsilonAlgorithm(PowerSeries<T> powerSeries)
         {
             _powerSeries = powerSeries;
+            _calculator = powerSeries.Calculator;
+            _epsilonPrevious = new T[powerSeries.NumberOfCoefficients + 1];
+            _epsilonCurrent = new T[powerSeries.NumberOfCoefficients];
+            _epsilonNext = new T[powerSeries.NumberOfCoefficients - 1];
         }
 
         public T Evaluate(T x)
         {
-            throw new NotImplementedException();
+            for (var i = 0; i < _epsilonPrevious.Count(); ++i)
+                _epsilonPrevious[i] = _calculator.AssignFromDouble(0);
+
+            for (var i = 0; i < _epsilonCurrent.Count(); ++i)
+            {
+                var xPotency = _calculator.Pow(x, i);
+                _epsilonCurrent[i] = _calculator.Multiply(_powerSeries[i], xPotency);
+            }
+
+            for (var i = 1; i <= _powerSeries.Degree; ++i)
+            {
+                for (var j = 0; j <= _powerSeries.Degree - i; ++j)
+                {
+                    var previousDifference = _calculator.Subtract(_epsilonCurrent[j + 1], _epsilonCurrent[j]);
+                    var invers = _calculator.Divide(_calculator.AssignFromDouble(1), previousDifference);
+                    _epsilonNext[j] = _calculator.Add(_epsilonPrevious[j + 1], invers);
+                }
+
+                var temp = _epsilonPrevious;
+                _epsilonPrevious = _epsilonCurrent;
+                _epsilonCurrent = _epsilonNext;
+                _epsilonNext = temp;
+            }
+
+            return _epsilonCurrent[0];
         }
 
         public T EvaluateAt1()
