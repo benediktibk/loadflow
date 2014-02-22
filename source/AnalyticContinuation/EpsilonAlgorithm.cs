23dd0d480d84877de7bdbf4f31ce28face13fb3a
Benedikt Schmidt <benediktibk@aon.at>
2014-02-22 19:53:26 +0100

    added structure for wynns epsilon algorithm

---
+++ source/AnalyticContinuation/EpsilonAlgorithm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AnalyticContinuation
+{
+    public class EpsilonAlgorithm<T> : IAnalyticContinuation<T> where T : struct, IEquatable<T>, IFormattable
+    {
+        private readonly PowerSeries<T> _powerSeries;
+ 
+        public EpsilonAlgorithm(PowerSeries<T> powerSeries)
+        {
+            _powerSeries = powerSeries;
+        }
+
+        public T Evaluate(T x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T EvaluateAt1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/AnalyticContinuationTest/EpsilonAlgorithmTest.cs
@@ -0,0 +1,14 @@
+﻿using AnalyticContinuation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnalyticContinuationTest
+{
+    [TestClass]
+    public class EpsilonAlgorithmTest : AnalyticContinuationTest
+    {
+        protected override IAnalyticContinuation<double> CreateAnalyticContinuation(PowerSeries<double> powerSeries)
+        {
+            return new EpsilonAlgorithm<double>(powerSeries);
+        }
+    }
+}
