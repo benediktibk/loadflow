f21db51d6bbbe8ef1361ed8c048712233468cc8c
Benedikt Schmidt <benediktibk@aon.at>
2014-02-24 21:59:50 +0100

    added structure for aitken process

---
+++ source/AnalyticContinuation/AitkenProcess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AnalyticContinuation
+{
+    public class AitkenProcess<T> : IAnalyticContinuation<T> where T : struct, IEquatable<T>, IFormattable
+    {
+        private readonly PowerSeries<T> _powerSeries;
+        private readonly ICalculatorGeneric<T> _calculator;
+
+        public AitkenProcess(PowerSeries<T> powerSeries)
+        {
+            _powerSeries = powerSeries;
+            _calculator = powerSeries.Calculator;
+        }
+
+        public T Evaluate(T x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T EvaluateAt1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/AnalyticContinuationTest/AitkenProcessTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+using AnalyticContinuation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnalyticContinuationTest
+{
+    [TestClass]
+    public class AitkenProcessTest : AnalyticContinuationTest
+    {
+        protected override IAnalyticContinuation<double> CreateAnalyticContinuation(PowerSeries<double> powerSeries)
+        {
+            return new AitkenProcess<double>(powerSeries);
+        }
+
+        protected override IAnalyticContinuation<Complex> CreateAnalyticContinuation(PowerSeries<Complex> powerSeries)
+        {
+            return new AitkenProcess<Complex>(powerSeries);
+        }
+    }
+}
