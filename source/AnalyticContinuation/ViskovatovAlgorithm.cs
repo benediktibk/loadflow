722fef0b2db0f16cb90eaeff000b98f4f5bb849f
Benedikt Schmidt <benediktibk@aon.at>
2014-02-25 15:51:23 +0100

    added structure for viskovatov algorithm

---
+++ source/AnalyticContinuation/ViskovatovAlgorithm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AnalyticContinuation
+{
+    public class ViskovatovAlgorithm<T> : IAnalyticContinuation<T> where T : struct, IEquatable<T>, IFormattable
+    {
+        public T Evaluate(T x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T EvaluateAt1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/AnalyticContinuationTest/ViskovatovAlgorithmTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+using AnalyticContinuation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnalyticContinuationTest
+{
+    [TestClass]
+    public class ViskovatovAlgorithmTest : AnalyticContinuationTest
+    {
+        protected override IAnalyticContinuation<double> CreateAnalyticContinuation(PowerSeries<double> powerSeries)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IAnalyticContinuation<Complex> CreateAnalyticContinuation(PowerSeries<Complex> powerSeries)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
