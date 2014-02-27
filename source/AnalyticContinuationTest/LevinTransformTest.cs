b57686201ba639dbfc81c63d863ea74f7c50a3d3
Benedikt Schmidt <benediktibk@aon.at>
2014-02-27 12:38:36 +0100

    added tests for levin transform

--- source/AnalyticContinuationTest/LevinTransformTest.cs
+++ source/AnalyticContinuationTest/LevinTransformTest.cs
@@ -3,6 +3,7 @@ using System.Numerics;
 using AnalyticContinuation;
 using MathExtensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestHelper;
 
 namespace AnalyticContinuationTest
 {
@@ -17,6 +18,40 @@ namespace AnalyticContinuationTest
         protected override IAnalyticContinuation<Complex> CreateAnalyticContinuation(PowerSeries<Complex> powerSeries)
         {
             return new LevinTransform<Complex>(powerSeries);
+        }
+
+        [TestMethod]
+        public void Evaluate_ExponentialAt2_ResultIsMoreAccurateThanDirectPowerSeries()
+        {
+            const double x = 2;
+            var correctValue = Math.Exp(x);
+            var directValue = _powerSeriesExponential.Evaluate(x);
+            var continuatedValue = _continuationExponential.Evaluate(x);
+            var directError = Math.Abs(directValue - correctValue);
+            var continuatedError = Math.Abs(continuatedValue - correctValue);
+
+            Assert.IsFalse(Double.IsNaN(directValue));
+            Assert.IsFalse(Double.IsInfinity(directValue));
+            Assert.IsFalse(Double.IsNaN(continuatedValue));
+            Assert.IsFalse(Double.IsInfinity(continuatedValue));
+            Assert.IsTrue(directError > continuatedError);
+        }
+
+        [TestMethod]
+        public void Evaluate_ExponentialAtMinus4_ResultIsMoreAccurateThanDirectPowerSeries()
+        {
+            const double x = -4;
+            var correctValue = Math.Exp(x);
+            var directValue = _powerSeriesExponential.Evaluate(x);
+            var continuatedValue = _continuationExponential.Evaluate(x);
+            var directError = Math.Abs(directValue - correctValue);
+            var continuatedError = Math.Abs(continuatedValue - correctValue);
+
+            Assert.IsFalse(Double.IsNaN(directValue));
+            Assert.IsFalse(Double.IsInfinity(directValue));
+            Assert.IsFalse(Double.IsNaN(continuatedValue));
+            Assert.IsFalse(Double.IsInfinity(continuatedValue));
+            Assert.IsTrue(directError > continuatedError);
         }
     }
 }
