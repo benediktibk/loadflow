d3e62847fb1e0ac73b439219708708a270e1bad1
Benedikt Schmidt <benediktibk@aon.at>
2014-02-27 10:42:53 +0100

    implemented BinomialCoefficient

--- source/MathExtensions/MathExtended.cs
+++ source/MathExtensions/MathExtended.cs
@@ -19,5 +19,25 @@ namespace MathExtensions
 
             return result;
         }
+
+        public static long BinomialCoefficient(long n, long k)
+        {
+            if (k < 0)
+                throw new ArgumentOutOfRangeException("k", "must be positive");
+
+            if (k > n)
+                throw new ArgumentOutOfRangeException("k", "must not be greater than n");
+
+            if (n == k || k == 0)
+                return 1;
+
+            var nominator = n;
+
+            for (var i = n - 1; i > n - k; --i)
+                nominator *= i;
+
+            var denominator = Factorial(k);
+            return nominator/denominator;
+        }
     }
 }
--- source/MathExtensionsTest/MathExtendedTest.cs
+++ source/MathExtensionsTest/MathExtendedTest.cs
@@ -31,5 +31,49 @@ namespace MathExtensionsTest
         {
             MathExtended.Factorial(-1);
         }
+
+        [TestMethod]
+        public void BinomialCoefficient_9And4_126()
+        {
+            Assert.AreEqual(126, MathExtended.BinomialCoefficient(9, 4));
+        }
+
+        [TestMethod]
+        public void BinomialCoefficient_5And3_10()
+        {
+            Assert.AreEqual(10, MathExtended.BinomialCoefficient(5, 3));
+        }
+
+        [TestMethod]
+        public void BinomialCoefficient_5And2_10()
+        {
+            Assert.AreEqual(10, MathExtended.BinomialCoefficient(5, 2));
+        }
+
+        [TestMethod]
+        public void BinomialCoefficient_9And9_1()
+        {
+            Assert.AreEqual(1, MathExtended.BinomialCoefficient(9, 9));
+        }
+
+        [TestMethod]
+        public void BinomialCoefficient_9And0_1()
+        {
+            Assert.AreEqual(1, MathExtended.BinomialCoefficient(9, 0));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void BinomialCoefficient_9And10_ExceptionThrown()
+        {
+            MathExtended.BinomialCoefficient(9, 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void BinomialCoefficient_9AndMinus1_ExceptionThrown()
+        {
+            MathExtended.BinomialCoefficient(9, -1);
+        }
     }
 }
