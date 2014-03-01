12ab07d38b84446d16c02f10c8fdae97f1c6a9d6
Benedikt Schmidt <benediktibk@aon.at>
2014-03-01 17:17:33 +0100

    added structure for newton-raphson method

---
+++ source/LoadFlowCalculation/NewtonRaphsonMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Numerics;
+using MathNet.Numerics.LinearAlgebra.Generic;
+
+namespace LoadFlowCalculation
+{
+    public class NewtonRaphsonMethod : LoadFlowCalculator
+    {
+        public override Vector<Complex> CalculateUnknownVoltages(Matrix<Complex> admittances, double nominalVoltage, Vector<Complex> constantCurrents, Vector<Complex> knownPowers,
+            out bool voltageCollapse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/LoadFlowCalculationTest/NewtonRaphsonMethodTest.cs
@@ -0,0 +1,14 @@
+﻿using LoadFlowCalculation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LoadFlowCalculationTest
+{
+    [TestClass]
+    public class NewtonRaphsonMethodTest : LoadFlowCalculatorHighAccuracyTest
+    {
+        protected override LoadFlowCalculator CreateLoadFlowCalculator()
+        {
+            return new NewtonRaphsonMethod();
+        }
+    }
+}
