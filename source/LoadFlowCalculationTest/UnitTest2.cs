d60322c4276cfaef856e260935c32e5e74849d73
Benedikt Schmidt <benediktibk@aon.at>
2014-03-03 20:56:28 +0100

    added class PV-Bus

---
+++ source/LoadFlowCalculationTest/UnitTest2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using LoadFlowCalculation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LoadFlowCalculationTest
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Constructor_NegativeID_ThrowsException()
+        {
+            var bus = new PVBus(-2, 3, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Constructor_NegativeVoltageMagnitude_ThrowsException()
+        {
+            var bus = new PVBus(0, 3, -1);
+        }
+
+        [TestMethod]
+        public void Constructor_IDSetTo1_IDIs1()
+        {
+            var bus = new PVBus(1, 2, 3);
+
+            Assert.AreEqual(1, bus.ID);
+        }
+
+        [TestMethod]
+        public void Constructor_RealPowerSetTo2_RealPowerIs2()
+        {
+            var bus = new PVBus(1, 2, 3);
+
+            Assert.AreEqual(2, bus.RealPower);
+        }
+
+        [TestMethod]
+        public void Constructor_VoltageMagnitudeSetTo3_VoltageMagnitudeIs3()
+        {
+            var bus = new PVBus(1, 2, 3);
+
+            Assert.AreEqual(3, bus.VoltageMagnitude);
+        }
+    }
+}
