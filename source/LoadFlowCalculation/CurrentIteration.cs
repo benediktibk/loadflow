09e0813c60a6306e6e0529f786db71b57e7c1e7e
Benedikt Schmidt <benediktibk@aon.at>
2014-03-04 09:52:47 +0100

    extracted function in current iteration where the inital voltage can be set

--- source/LoadFlowCalculation/CurrentIteration.cs
+++ source/LoadFlowCalculation/CurrentIteration.cs
@@ -16,20 +16,16 @@ namespace LoadFlowCalculation
             _maximumIterations = maximumIterations;
         }
 
-        public override Vector<Complex> CalculateUnknownVoltages(Matrix<Complex> admittances, double nominalVoltage, Vector<Complex> constantCurrents, IList<PQBus> pqBuses, IList<PVBus> pvBuses, out bool voltageCollapse)
+        public Vector<Complex> CalculateUnknownVoltages(Matrix<Complex> admittances, double nominalVoltage,
+            Vector<Complex> constantCurrents, IList<PQBus> pqBuses, IList<PVBus> pvBuses, out bool voltageCollapse,
+            Vector<Complex> initialVoltages)
         {
             var knownPowers = new DenseVector(pqBuses.Count);
 
             foreach (var bus in pqBuses)
                 knownPowers[bus.ID] = bus.Power;
 
-            var nodeCount = admittances.RowCount;
-            var initialVoltages = new Complex[nodeCount];
-
-            for (var i = 0; i < nodeCount; ++i)
-                initialVoltages[i] = new Complex(nominalVoltage, 0);
-
-            Vector<Complex> voltages = new DenseVector(initialVoltages);
+            var voltages = initialVoltages;
             var iterations = 0;
             var knownPowersConjugated = knownPowers.Conjugate();
             var factorization = admittances.QR();
@@ -42,13 +38,25 @@ namespace LoadFlowCalculation
                 var newVoltages = factorization.Solve(currents);
                 var voltageDifference = newVoltages.Subtract(voltages);
                 var maximumVoltageDifference = voltageDifference.AbsoluteMaximum();
-                voltageChange = maximumVoltageDifference.Magnitude/nominalVoltage;
+                voltageChange = maximumVoltageDifference.Magnitude / nominalVoltage;
                 voltages = newVoltages;
                 ++iterations;
             } while (iterations <= _maximumIterations && voltageChange > _terminationCriteria);
 
             voltageCollapse = iterations > _maximumIterations;
             return voltages;
+        }
+
+        public override Vector<Complex> CalculateUnknownVoltages(Matrix<Complex> admittances, double nominalVoltage, Vector<Complex> constantCurrents, IList<PQBus> pqBuses, IList<PVBus> pvBuses, out bool voltageCollapse)
+        {
+            var nodeCount = admittances.RowCount;
+            var initialVoltages = new DenseVector(nodeCount);
+
+            for (var i = 0; i < nodeCount; ++i)
+                initialVoltages[i] = new Complex(nominalVoltage, 0);
+
+            return CalculateUnknownVoltages(admittances, nominalVoltage, constantCurrents, pqBuses, pvBuses,
+                out voltageCollapse, initialVoltages);
         }
     }
 }
