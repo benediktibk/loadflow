9e73e8e763c9ef26873e7c4bd9eb3173f7caf6fb
Benedikt Schmidt <benediktibk@aon.at>
2014-03-18 12:38:34 +0100

    added tests for complex multiprecision

--- source/LoadFlowCalculationAccuracyImprovement/UnitTest.cpp
+++ source/LoadFlowCalculationAccuracyImprovement/UnitTest.cpp
@@ -7,7 +7,13 @@ bool areEqual(Complex<double> const& one, Complex<double> const& two, double del
 			std::abs(one.imag() - two.imag()) < delta;
 }
 
-bool runTestsComplex()
+bool areEqual(Complex<MultiPrecision> const& one, Complex<MultiPrecision> const& two, double delta)
+{
+	return	std::abs(one.real() - two.real()) < MultiPrecision(delta) &&
+			std::abs(one.imag() - two.imag()) < MultiPrecision(delta);
+}
+
+bool runTestsComplexDouble()
 {
 	Complex<double> one(2, 3);
 	Complex<double> two(5, 7);
@@ -34,7 +40,40 @@ bool runTestsComplex()
 	return true;
 }
 
+bool runTestsComplexMultiPrecision()
+{
+	Complex<MultiPrecision> one(MultiPrecision(2), MultiPrecision(3));
+	Complex<MultiPrecision> two(MultiPrecision(5), MultiPrecision(7));
+
+	Complex<MultiPrecision> add = one + two;
+	Complex<MultiPrecision> subtract = one - two;
+	Complex<MultiPrecision> multiply = one * two;
+	Complex<MultiPrecision> divide = one / two;
+
+	Complex<MultiPrecision> addExpected(MultiPrecision(7), MultiPrecision(10));
+	Complex<MultiPrecision> subtractExpected(MultiPrecision(-3), MultiPrecision(-4));
+	Complex<MultiPrecision> multiplyExpected(MultiPrecision(-11), MultiPrecision(29));
+	Complex<MultiPrecision> divideExpected(MultiPrecision(0.418918918), MultiPrecision(0.013513513));
+
+	if (!areEqual(addExpected, add, 0.0001))
+		return false;
+	if (!areEqual(subtractExpected, subtract, 0.0001))
+		return false;
+	if (!areEqual(multiplyExpected, multiply, 0.0001))
+		return false;
+	if (!areEqual(divideExpected, divide, 0.0001))
+		return false;
+
+	return true;
+}
+
 bool runTests()
 {
-	return runTestsComplex();
+	if (!runTestsComplexDouble())
+		return false;
+
+	if (!runTestsComplexMultiPrecision())
+		return false;
+
+	return true;
 }
