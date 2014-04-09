ac167db88a72ed64bb55cfd7e7d17002aa68650e
Benedikt Schmidt <benediktibk@aon.at>
2014-04-09 17:07:12 +0200

    simplified tests for Load

--- source/LoadFlowCalculationTest/MultipleVoltageLevels/LoadTest.cs
+++ source/LoadFlowCalculationTest/MultipleVoltageLevels/LoadTest.cs
@@ -8,49 +8,38 @@ namespace LoadFlowCalculationTest.MultipleVoltageLevels
     [TestClass]
     public class LoadTest
     {
-        [TestMethod]
-        public void Constructor_hanzAsName_NameIshanz()
-        {
-            var node = new Node("heinz", 4);
-            var load = new Load("hanz", new Complex(4, 1), node);
+        private Node _node;
+        private Load _load;
 
-            Assert.AreEqual("hanz", load.Name);
+        [TestInitialize]
+        public void SetUp()
+        {
+            _node = new Node("heinz", 3);
+            _load = new Load("hanz", new Complex(4, 1), _node);
         }
 
         [TestMethod]
-        public void Constructor_4AsNominalVoltage_NominalVoltageIs4()
+        public void Constructor_hanzAsName_NameIshanz()
         {
-            var node = new Node("heinz", 4);
-            var load = new Load("hanz", new Complex(4, 1), node);
-
-            Assert.AreEqual(4, load.NominalVoltage, 0.00001);
+            Assert.AreEqual("hanz", _load.Name);
         }
 
         [TestMethod]
         public void Constructor_ValidLoad_LoadIsCorrect()
         {
-            var node = new Node("heinz", 4);
-            var load = new Load("hanz", new Complex(4, 1), node);
-
-            ComplexAssert.AreEqual(4, 1, load.Value, 0.00001);
+            ComplexAssert.AreEqual(4, 1, _load.Value, 0.00001);
         }
 
         [TestMethod]
         public void Constructor_ValidNode_NodeHasNoConnectedElements()
         {
-            var node = new Node("heinz", 4);
-            var load = new Load("hanz", new Complex(4, 1), node);
-
-            Assert.AreEqual(0, node.ConnectedElements.Count);
+            Assert.AreEqual(0, _node.ConnectedElements.Count);
         }
 
         [TestMethod]
         public void NominalVoltage_NodeHasNominalVoltageOf3_3()
         {
-            var node = new Node("heinz", 3);
-            var load = new Load("hanz", new Complex(4, 1), node);
-
-            Assert.AreEqual(3, load.NominalVoltage, 0.00001);
+            Assert.AreEqual(3, _load.NominalVoltage, 0.00001);
         }
     }
 }
