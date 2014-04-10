7699c5f1389dea74204801180997b63485a6e5ce
Benedikt Schmidt <benediktibk@aon.at>
2014-04-10 15:39:36 +0200

    implemented FeedIn

--- source/LoadFlowCalculation/MultipleVoltageLevels/FeedIn.cs
+++ source/LoadFlowCalculation/MultipleVoltageLevels/FeedIn.cs
@@ -1,6 +1,33 @@
-﻿namespace LoadFlowCalculation.MultipleVoltageLevels
+﻿using System.Numerics;
+
+namespace LoadFlowCalculation.MultipleVoltageLevels
 {
     public class FeedIn
     {
+        private readonly string _name;
+        private readonly Node _node;
+        private readonly Complex _voltage;
+
+        public FeedIn(string name, Node node, Complex voltage)
+        {
+            _name = name;
+            _node = node;
+            _voltage = voltage;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public double NominalVoltage
+        {
+            get { return _node.NominalVoltage; }
+        }
+
+        public Complex Voltage
+        {
+            get { return _voltage; }
+        }
     }
 }
--- source/LoadFlowCalculationTest/MultipleVoltageLevels/FeedInTest.cs
+++ source/LoadFlowCalculationTest/MultipleVoltageLevels/FeedInTest.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using System.Numerics;
+using LoadFlowCalculation.MultipleVoltageLevels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestHelper;
 
 namespace LoadFlowCalculationTest.MultipleVoltageLevels
 {
     [TestClass]
     public class FeedInTest
     {
+        private Node _node;
+        private FeedIn _feedIn;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _node = new Node("nnnode", 1000);
+            _feedIn = new FeedIn("feedIt", _node, new Complex(4, 3));
+        }
+
+        [TestMethod]
+        public void Constructor_NameSetTofeedIt_NameIsfeedIt()
+        {
+            Assert.AreEqual("feedIt", _feedIn.Name);
+        }
+
+        [TestMethod]
+        public void Constructor_ValidVoltageSet_VoltageIsCorrect()
+        {
+            ComplexAssert.AreEqual(new Complex(4, 3), _feedIn.Voltage, 0.0001);
+        }
+
+        [TestMethod]
+        public void NominalVoltage_Empty_SameAsNode()
+        {
+            Assert.AreEqual(1000, _feedIn.NominalVoltage, 0.00001);
+        }
     }
 }
