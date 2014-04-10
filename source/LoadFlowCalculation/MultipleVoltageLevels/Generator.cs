c53547d0685fb79a0c72316cee67566d4bdcfcbe
Benedikt Schmidt <benediktibk@aon.at>
2014-04-10 15:44:55 +0200

    implemented basics for generator

--- source/LoadFlowCalculation/MultipleVoltageLevels/Generator.cs
+++ source/LoadFlowCalculation/MultipleVoltageLevels/Generator.cs
@@ -2,5 +2,23 @@
 {
     public class Generator
     {
+        private readonly string _name;
+        private readonly Node _node;
+
+        public Generator(string name, Node node)
+        {
+            _name = name;
+            _node = node;
+        }
+
+        public string Name 
+        {
+            get { return _name; }
+        }
+
+        public double NominalVoltage
+        {
+            get { return _node.NominalVoltage; }
+        }
     }
 }
--- source/LoadFlowCalculationTest/MultipleVoltageLevels/GeneratorTest.cs
+++ source/LoadFlowCalculationTest/MultipleVoltageLevels/GeneratorTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using LoadFlowCalculation.MultipleVoltageLevels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LoadFlowCalculationTest.MultipleVoltageLevels
@@ -6,5 +6,26 @@ namespace LoadFlowCalculationTest.MultipleVoltageLevels
     [TestClass]
     public class GeneratorTest
     {
+        private Node _node;
+        private Generator _generator;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _node = new Node("ultimateNode", 103);
+            _generator = new Generator("dada", _node);
+        }
+
+        [TestMethod]
+        public void Constructor_NameSetTodada_NameIsdada()
+        {
+            Assert.AreEqual("dada", _generator.Name);
+        }
+
+        [TestMethod]
+        public void NominalVoltage_Empty_103()
+        {
+            Assert.AreEqual(103, _generator.NominalVoltage, 0.00001);
+        }
     }
 }
