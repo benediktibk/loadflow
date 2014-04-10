6351a14b64575b921a8e3429a74ec2ca2bbc47e9
Benedikt Schmidt <benediktibk@aon.at>
2014-04-10 16:07:46 +0200

    implemented basics for Line

---
+++ source/LoadFlowCalculation/MultipleVoltageLevels/Line.cs
@@ -0,0 +1,31 @@
+﻿namespace LoadFlowCalculation.MultipleVoltageLevels
+{
+    public class Line
+    {
+        private readonly string _name;
+        private readonly Node _sourceNode;
+        private readonly Node _targetNode;
+
+        public Line(string name, Node sourceNode, Node targetNode)
+        {
+            _name = name;
+            _sourceNode = sourceNode;
+            _targetNode = targetNode;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public double SourceNominalVoltage
+        {
+            get { return _sourceNode.NominalVoltage; }
+        }
+
+        public double TargetNominalVoltage
+        {
+            get { return _targetNode.NominalVoltage; }
+        }
+    }
+}
---
+++ source/LoadFlowCalculationTest/MultipleVoltageLevels/LineTest.cs
@@ -0,0 +1,39 @@
+﻿using LoadFlowCalculation.MultipleVoltageLevels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LoadFlowCalculationTest.MultipleVoltageLevels
+{
+    [TestClass]
+    public class LineTest
+    {
+        private Line _line;
+        private Node _sourceNode;
+        private Node _targetNode;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _sourceNode = new Node("source", 102);
+            _targetNode = new Node("target", 12);
+            _line = new Line("connect", _sourceNode, _targetNode);
+        }
+
+        [TestMethod]
+        public void Constructor_NameSetToconnect_NameIsconnect()
+        {
+            Assert.AreEqual("connect", _line.Name);
+        }
+
+        [TestMethod]
+        public void SourceNominalVoltage_SourceNodeVoltageSetTo102_102()
+        {
+            Assert.AreEqual(102, _line.SourceNominalVoltage, 0.00001);
+        }
+
+        [TestMethod]
+        public void TargetNominalVoltage_TargetNodeVoltageSetTo12_12()
+        {
+            Assert.AreEqual(12, _line.TargetNominalVoltage, 0.00001);
+        }
+    }
+}
