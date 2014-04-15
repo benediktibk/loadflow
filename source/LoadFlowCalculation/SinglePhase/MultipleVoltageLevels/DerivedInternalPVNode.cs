637dab6dbd260fabeff531b84d865ada25b2dc02
Benedikt Schmidt <benediktibk@aon.at>
2014-04-15 21:26:06 +0200

    implemented derived internal pv node

---
+++ source/LoadFlowCalculation/SinglePhase/MultipleVoltageLevels/DerivedInternalPVNode.cs
@@ -0,0 +1,24 @@
+﻿namespace LoadFlowCalculation.SinglePhase.MultipleVoltageLevels
+{
+    public class DerivedInternalPVNode : DerivedInternalNode
+    {
+        private readonly double _voltageMagnitude;
+        private readonly double _realPower;
+
+        public DerivedInternalPVNode(IExternalReadOnlyNode sourceNode, string name, double voltageMagnitude, double realPower) : base(sourceNode, name)
+        {
+            _voltageMagnitude = voltageMagnitude;
+            _realPower = realPower;
+        }
+
+        public override SingleVoltageLevel.Node CreateSingleVoltageNode(double scaleBasePower)
+        {
+            var scaler = new DimensionScaler(NominalVoltage, scaleBasePower);
+            return new SingleVoltageLevel.Node()
+            {
+                VoltageMagnitude = scaler.ScaleVoltage(_voltageMagnitude),
+                RealPower = scaler.ScalePower(_realPower)
+            };
+        }
+    }
+}
---
+++ source/LoadFlowCalculationTest/SinglePhase/MultipleVoltageLevels/DerivedInternalPVNodeTest.cs
@@ -0,0 +1,27 @@
+﻿using LoadFlowCalculation.SinglePhase.MultipleVoltageLevels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LoadFlowCalculationTest.SinglePhase.MultipleVoltageLevels
+{
+    [TestClass]
+    public class DerivedInternalPVNodeTest : DerivedInternalNodeTest
+    {
+        protected override DerivedInternalNode CreateNode(Node sourceNode, string name)
+        {
+            return new DerivedInternalPVNode(sourceNode, name, 5, 4);
+        }
+
+        [TestMethod]
+        public void CreateSingleVoltageNode_ValidScaleBases_VoltageOfPVBusIsCorrectScaled()
+        {
+            var sourceNode = new Node("source", 3);
+            var node = CreateNode(sourceNode, "pqbus");
+
+            var result = node.CreateSingleVoltageNode(2);
+
+            Assert.IsTrue(result.IsPVBus);
+            Assert.AreEqual(5.0 / 3, result.VoltageMagnitude, 0.00001);
+            Assert.AreEqual(2, result.RealPower, 0.00001);
+        }
+    }
+}
