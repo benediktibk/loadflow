d4215227cc3c242ec180c75eff64f28200ad49e3
Benedikt Schmidt <benediktibk@aon.at>
2014-04-15 21:20:29 +0200

    implemented derived internal slack node

---
+++ source/LoadFlowCalculation/SinglePhase/MultipleVoltageLevels/DerivedInternalSlackNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace LoadFlowCalculation.SinglePhase.MultipleVoltageLevels
+{
+    public class DerivedInternalSlackNode : DerivedInternalNode
+    {
+        private readonly Complex _voltage;
+
+        public DerivedInternalSlackNode(IExternalReadOnlyNode sourceNode, string name, Complex voltage) : base(sourceNode, name)
+        {
+            _voltage = voltage;
+        }
+
+        public override SingleVoltageLevel.Node CreateSingleVoltageNode(double scaleBasePower)
+        {
+            var scaler = new DimensionScaler(NominalVoltage, scaleBasePower);
+            return new SingleVoltageLevel.Node() {Voltage = scaler.ScaleVoltage(_voltage)};
+        }
+    }
+}
---
+++ source/LoadFlowCalculationTest/SinglePhase/MultipleVoltageLevels/DerivedInternalSlackNodeTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+using LoadFlowCalculation.SinglePhase.MultipleVoltageLevels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestHelper;
+
+namespace LoadFlowCalculationTest.SinglePhase.MultipleVoltageLevels
+{
+    [TestClass]
+    public class DerivedInternalSlackNodeTest : DerivedInternalNodeTest
+    {
+        protected override DerivedInternalNode CreateNode(Node sourceNode, string name)
+        {
+            return new DerivedInternalSlackNode(sourceNode, name, new Complex(10, 1));
+        }
+
+        [TestMethod]
+        public void CreateSingleVoltageNode_3AsScaleBaseForVoltage_VoltageOfPVBusIsCorrectScaled()
+        {
+            var sourceNode = new Node("source", 3);
+            var node = CreateNode(sourceNode, "pqbus");
+
+            var result = node.CreateSingleVoltageNode(2);
+
+            Assert.IsTrue(result.IsSlackBus);
+            ComplexAssert.AreEqual(10.0/3, 1.0/3, result.Voltage, 0.00001);
+        }
+    }
+}
