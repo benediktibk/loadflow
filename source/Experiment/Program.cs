103fdf06ccbf043035bce14cfee5225b42cd3d66
Benedikt Schmidt <benediktibk@aon.at>
2014-07-13 08:40:53 +0200

    changed experiment to test the admittance matrix in case of an transformer

--- source/Experiment/Program.cs
+++ source/Experiment/Program.cs
@@ -1,161 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Numerics;
-using AnalyticContinuation;
-using MathExtensions;
+using LoadFlowCalculation.SinglePhase.MultipleVoltageLevels;
 using MathNet.Numerics;
 using MathNet.Numerics.LinearAlgebra.Complex;
-using MathNet.Numerics.LinearAlgebra.Complex.Factorization;
-using MathNet.Numerics.LinearAlgebra.Generic;
 
 namespace Experiment
 {
     class Program
     {
-        private static IList<Complex> _coefficientsVoltageOne;
-        private static IList<Complex> _coefficientsVoltageOneSquared;
-        private static IList<Complex> _coefficientsVoltageOneWeighted;
-        private static IList<Complex> _coefficientsVoltageTwo;
-        private static IList<Complex> _coefficientsVoltageTwoInverse;
-        private static IList<Complex> _coefficientsVoltageTwoConjugated;
-        private static Matrix<Complex> _admittanceMatrixReduced;
-        private static Complex _admittanceOneZero;
-        private static Complex _admittanceTwoZero;
-        private static Complex _slackVoltage;
-        private static double _powerOne;
-        private static Complex _powerTwo;
-        private static double _voltageMagnitudeOne;
-        private static QR _factorization;
-
         static void Main(string[] args)
         {
-            const int coefficientCount = 60;
-            _coefficientsVoltageOne = new List<Complex>(coefficientCount);
-            _coefficientsVoltageOneSquared = new List<Complex>(coefficientCount);
-            _coefficientsVoltageOneWeighted = new List<Complex>(coefficientCount);
-            _coefficientsVoltageTwo = new List<Complex>(coefficientCount);
-            _coefficientsVoltageTwoInverse = new List<Complex>(coefficientCount);
-            _coefficientsVoltageTwoConjugated = new List<Complex>(coefficientCount);
-            _admittanceMatrixReduced =
-                DenseMatrix.OfArray(new[,]
-                {{new Complex(15, 1500), new Complex(-10, -1000)}, {new Complex(-10, -1000), new Complex(30, 700)}});
-            _factorization = _admittanceMatrixReduced.QR();
-            _admittanceOneZero = new Complex(-5, -500);
-            _admittanceTwoZero = new Complex(-20, 300);
-            _slackVoltage = new Complex(1, 0.12);
-            _powerOne = -46.86;
-            _powerTwo = new Complex(30.23, -58.52);
-            _voltageMagnitudeOne = Math.Sqrt(0.9*0.9 + 0.1*0.1);
-
-            for (var i = 0; i < coefficientCount; ++i)
-                CalculateNextCoefficients();
-
-            var powerSeriesVoltageOne = new PowerSeriesComplex(coefficientCount);
-            var powerSeriesVoltageTwo = new PowerSeriesComplex(coefficientCount);
-
-            for (var i = 0; i < coefficientCount; ++i)
+            for (var j = -20; j <= 20; ++j)
             {
-                powerSeriesVoltageOne[i] = _coefficientsVoltageOne[i];
-                powerSeriesVoltageTwo[i] = _coefficientsVoltageTwo[i];
+                var powerNet = new PowerNet(50);
+                powerNet.AddNode("sourceNode", 1000);
+                powerNet.AddNode("targetNode", 400);
+                powerNet.AddFeedIn("sourceNode", "feedIn", new Complex(1002, 0), 0);
+                powerNet.AddTransformer("sourceNode", "targetNode", "transformer", new Complex(9.410964912, 0),
+                    new Complex(0.399, 0), new Complex(), 2.49999, Math.Pow(10, j));
+                powerNet.AddLoad("targetNode", "load", new Complex(-2000, 0));
+
+                var nodes = new List<IReadOnlyNode>(powerNet.GetAllNodes());
+                IReadOnlyNode groundNode = null;
+
+                if (powerNet.CheckIfGroundNodeIsNecessary())
+                {
+                    groundNode = new Node("#ground", 0);
+                    nodes.Add(groundNode);
+                }
+
+                var nodeIndexes = new Dictionary<IReadOnlyNode, int>();
+
+                for (var i = 0; i < nodes.Count; ++i)
+                    nodeIndexes.Add(nodes[i], i);
+
+                var admittances = new AdmittanceMatrix(nodes.Count, nodeIndexes);
+                powerNet.FillInAdmittances(admittances, 1, groundNode);
+                var matrix = admittances.GetValues();
+                var matrixReduced = new DenseMatrix(matrix.RowCount - 1, matrix.ColumnCount - 1);
+                for (var row = 0; row < matrix.RowCount - 1; ++row)
+                    for (var column = 0; column < matrix.ColumnCount - 1; ++column)
+                        matrixReduced[row, column] = matrix[row + 1, column + 1];
+
+                var separation = matrixReduced.LU();
+                var norm = matrixReduced.L2Norm();
+                var inverse = separation.Inverse();
+
+                try
+                {
+                    var inverseNorm = inverse.L2Norm();
+                    var condition = (norm*inverseNorm).Magnitude;
+                    Console.WriteLine(j + " -> " + condition);
+                }
+                catch (NonConvergenceException exception)
+                {
+                    Console.WriteLine(j + " -> not converging");
+                }
             }
 
-            var continuationVoltageOne = new EpsilonAlgorithm<Complex>(powerSeriesVoltageOne);
-            var continuationVoltageTwo = new EpsilonAlgorithm<Complex>(powerSeriesVoltageTwo);
-            var voltageOne = continuationVoltageOne.EvaluateAt1();
-            var correctOne = Math.Abs(0.9 - voltageOne.Real) < 0.001 && Math.Abs(0.1 - voltageOne.Imaginary) < 0.001;
-            var voltageTwo = continuationVoltageTwo.EvaluateAt1();
-            var correctTwo = Math.Abs(0.95 - voltageTwo.Real) < 0.001 && Math.Abs(0.05 - voltageTwo.Imaginary) < 0.001;
-            Console.WriteLine("result voltage one: " + voltageOne);
-            Console.WriteLine(correctOne ? "the result is correct" : "the result is incorrect");
-            Console.WriteLine("result voltage two: " + voltageTwo);
-            Console.WriteLine(correctTwo ? "the result is correct" : "the result is incorrect");
             Console.ReadKey();
-        }
-
-        static void CalculateNextCoefficients()
-        {
-            if (_coefficientsVoltageOne.Count == 0)
-                CalculateFirstCoefficients();
-            else
-                CalculateNextCoefficientsInternal();
-
-            CalculateNextDerivedCoefficients();
-        }
-
-        private static void CalculateNextDerivedCoefficients()
-        {
-            CalculateNextInverseCoefficient();
-            CalculateNextSquaredCoefficient();
-            CalculateNextConjugatedCoefficient();
-            CalculateNextWeightedCoefficient();
-        }
-
-        private static void CalculateNextWeightedCoefficient()
-        {
-            _coefficientsVoltageOneWeighted.Add(CalculateConvolution(_coefficientsVoltageOneSquared, _coefficientsVoltageTwoConjugated));
-        }
-
-        private static void CalculateNextConjugatedCoefficient()
-        {
-            _coefficientsVoltageTwoConjugated.Add(_coefficientsVoltageTwo.Last().Conjugate());
-        }
-
-        private static void CalculateNextSquaredCoefficient()
-        {
-            _coefficientsVoltageOneSquared.Add(CalculateConvolution(_coefficientsVoltageOne, _coefficientsVoltageOne));
-        }
-
-        private static Complex CalculateConvolution(IList<Complex> one, IList<Complex> two)
-        {
-            var n = one.Count - 1;
-            var result = new Complex();
-
-            for (var i = 0; i <= n; ++i)
-                result += one[i]*two[n - i];
-
-            return result;
-        }
-
-        private static void CalculateNextInverseCoefficient()
-        {
-	        var n = _coefficientsVoltageTwo.Count - 1;
-
-            if (n == 0)
-            {
-                _coefficientsVoltageTwoInverse.Add(1/_coefficientsVoltageTwo[0]);
-                return;
-            }
-            
-            var result = new Complex();
-	        for (var i = 0; i < n; ++i)
-		        result -= _coefficientsVoltageTwo[n - i]*_coefficientsVoltageTwoInverse[i];
-
-            _coefficientsVoltageTwoInverse.Add(result/_coefficientsVoltageTwo[0]);
-        }
-
-        private static void CalculateNextCoefficientsInternal()
-        {
-            var rightHandSide = new DenseVector(2);
-            var voltageMagnitudeSquare = _voltageMagnitudeOne*_voltageMagnitudeOne;
-            rightHandSide[0] = (2*_powerOne/voltageMagnitudeSquare - _admittanceMatrixReduced[0, 0].Conjugate())*
-                               _coefficientsVoltageOne.Last() -
-                               _admittanceOneZero.Conjugate()*_slackVoltage.Conjugate()/voltageMagnitudeSquare*
-                               _coefficientsVoltageOneSquared.Last() -
-                               _admittanceMatrixReduced[0, 1].Conjugate()/voltageMagnitudeSquare*
-                               _coefficientsVoltageOneWeighted.Last();
-            rightHandSide[1] = (_powerTwo*_coefficientsVoltageTwoInverse.Last()).Conjugate();
-            var coefficients = _factorization.Solve(rightHandSide);
-            _coefficientsVoltageOne.Add(coefficients[0]);
-            _coefficientsVoltageTwo.Add(coefficients[1]);
-        }
-
-        private static void CalculateFirstCoefficients()
-        {
-            var rightHandSide = new DenseVector(new[] {(-1)*_admittanceOneZero*_slackVoltage, (-1)*_admittanceTwoZero*_slackVoltage});
-            var coefficients = _factorization.Solve(rightHandSide);
-            _coefficientsVoltageOne.Add(coefficients[0]);
-            _coefficientsVoltageTwo.Add(coefficients[1]);
         }
     }
 }
