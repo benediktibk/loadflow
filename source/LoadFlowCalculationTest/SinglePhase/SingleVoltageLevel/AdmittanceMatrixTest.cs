0bae5d5edd2161554ab5dff14e7953cd3d680944
Benedikt Schmidt <benediktibk@aon.at>
2014-07-21 18:58:16 +0200

    added test for admittance matrix

--- source/LoadFlowCalculationTest/SinglePhase/SingleVoltageLevel/AdmittanceMatrixTest.cs
+++ source/LoadFlowCalculationTest/SinglePhase/SingleVoltageLevel/AdmittanceMatrixTest.cs
@@ -55,5 +55,32 @@ namespace LoadFlowCalculationTest.SinglePhase.SingleVoltageLevel
             ComplexAssert.AreEqual(0, 0, totalCurrents[2], 0.00001);
             ComplexAssert.AreEqual(-1, 0, totalCurrents[3], 0.00001);
         }
+
+        [TestMethod]
+        public void CreateReducedAdmittanceMatrix_OneIdealTransformerWithTwoConnectionsAndInputVoltageKnownAndNoLoad_CurrentsAreCorrect()
+        {
+            _admittances = new AdmittanceMatrix(6);
+            _admittances.AddConnection(1, 2, new Complex(0.1, 0));
+            _admittances.AddConnection(4, 5, new Complex(0.1, 0));
+            _admittances.AddIdealTransformer(2, 0, 4, 0, 3, 2, 1);
+            _knownVoltageIndex.Add(0);
+            _knownVoltageIndex.Add(1);
+            _unknownVoltageIndex.Add(2);
+            _unknownVoltageIndex.Add(3);
+            _unknownVoltageIndex.Add(4);
+            _unknownVoltageIndex.Add(5);
+            _knownVoltages = new DenseVector(new[] { new Complex(0, 0), new Complex(30, 0) });
+
+            var reducedMatrix = _admittances.CreateReducedAdmittanceMatrix(_unknownVoltageIndex, _knownVoltageIndex,
+                _knownVoltages, out _constantCurrents);
+
+            _unknownVoltages = new DenseVector(new[] { new Complex(30, 0), new Complex(0, 0), new Complex(15, 0), new Complex(15, 0) });
+            var branchCurrents = reducedMatrix.CalculateCurrents(_unknownVoltages);
+            var totalCurrents = branchCurrents - _constantCurrents;
+            ComplexAssert.AreEqual(0, 0, totalCurrents[0], 0.00001);
+            ComplexAssert.AreEqual(0, 0, totalCurrents[1], 0.00001);
+            ComplexAssert.AreEqual(0, 0, totalCurrents[2], 0.00001);
+            ComplexAssert.AreEqual(0, 0, totalCurrents[3], 0.00001);
+        }
     }
 }
