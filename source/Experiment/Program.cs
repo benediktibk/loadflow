751401908dd46e0cf9df78dcdf893b25fcc95371
Benedikt Schmidt <benediktibk@aon.at>
2014-07-23 11:41:09 +0200

    tested sparse LU decomposition of complex matrices

--- source/Experiment/Program.cs
+++ source/Experiment/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Numerics;
+using MathNet.Numerics.LinearAlgebra.Complex;
 
 namespace Experiment
 {
@@ -6,7 +8,15 @@ namespace Experiment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("nothing to do");
+            var A = new SparseMatrix(2, 2);
+            A[0, 1] = new Complex(1, 0);
+            A[1, 0] = new Complex(1, 0);
+            var b = new SparseVector(2);
+            b[0] = new Complex(1, 0);
+            b[1] = new Complex(2, 0);
+            var factorization = A.LU();
+            var x = factorization.Solve(b);
+            Console.WriteLine(x.ToString());
             Console.ReadKey();
         }
     }
