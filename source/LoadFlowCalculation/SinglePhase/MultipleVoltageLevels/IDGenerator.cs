663c9e3f69dd835bc75dab034c5d564ce5e6117e
Benedikt Schmidt <benediktibk@aon.at>
2014-07-23 10:27:50 +0200

    implemented ID generator

---
+++ source/LoadFlowCalculation/SinglePhase/MultipleVoltageLevels/IDGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoadFlowCalculation.SinglePhase.MultipleVoltageLevels
+{
+    public class IdGenerator
+    {
+        private readonly HashSet<long> _inUse;
+        private long _creationCounter;
+
+        public IdGenerator()
+        {
+            _inUse = new HashSet<long>();
+            _creationCounter = 0;
+        }
+
+        public bool IsAlreadyUsed(long id)
+        {
+            return _inUse.Contains(id);
+        }
+
+        public void Add(long id)
+        {
+            if (id < 0)
+                throw new ArgumentOutOfRangeException("id", "must not be negative");
+
+            if (IsAlreadyUsed(id))
+                throw new ArgumentException("id is already in use");
+
+            _inUse.Add(id);
+        }
+
+        public long Generate()
+        {
+            if (_creationCounter == long.MinValue)
+                throw new InvalidOperationException("no free ids left");
+
+            --_creationCounter;
+            _inUse.Add(_creationCounter);
+            return _creationCounter;
+        }
+
+        public long Count
+        {
+            get { return _inUse.Count; }
+        }
+    }
+}
---
+++ source/LoadFlowCalculationTest/SinglePhase/MultipleVoltageLevels/IdGeneratorTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using LoadFlowCalculation.SinglePhase.MultipleVoltageLevels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LoadFlowCalculationTest.SinglePhase.MultipleVoltageLevels
+{
+    [TestClass]
+    public class IdGeneratorTest
+    {
+        private IdGenerator _generator;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _generator = new IdGenerator();
+        }
+
+        [TestMethod]
+        public void Constructor_Empty_CountIs0()
+        {
+            Assert.AreEqual(0, _generator.Count);
+        }
+
+        [TestMethod]
+        public void Add_5_CountIs1()
+        {
+            _generator.Add(5);
+
+            Assert.AreEqual(1, _generator.Count);
+        }
+
+        [TestMethod]
+        public void Add_ThreeTimesCalled_CountIs3()
+        {
+            _generator.Add(0);
+            _generator.Add(5);
+            _generator.Add(2);
+
+            Assert.AreEqual(3, _generator.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Add_SameIdTwiceCalled_ExceptionIsThrown()
+        {
+            _generator.Add(5);
+            _generator.Add(5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Add_Minus2_ExceptionIsThrown()
+        {
+            _generator.Add(-2);
+        }
+
+        [TestMethod]
+        public void Generate_ThriceCalled_NegativeValues()
+        {
+            Assert.IsTrue(_generator.Generate() < 0);
+            Assert.IsTrue(_generator.Generate() < 0);
+            Assert.IsTrue(_generator.Generate() < 0);
+        }
+
+        [TestMethod]
+        public void Generate_ThriceCalled_ThreeDifferentValues()
+        {
+            var first = _generator.Generate();
+            var second = _generator.Generate();
+            var third = _generator.Generate();
+
+            Assert.AreNotEqual(first, second);
+            Assert.AreNotEqual(first, third);
+            Assert.AreNotEqual(second, third);
+        }
+
+        [TestMethod]
+        public void Generate_OnceCalled_ResultIsInUse()
+        {
+            var value = _generator.Generate();
+
+            Assert.IsTrue(_generator.IsAlreadyUsed(value));
+        }
+
+        [TestMethod]
+        public void IsAlreadyUsed_NothingAdded_False()
+        {
+            Assert.IsFalse(_generator.IsAlreadyUsed(4));
+        }
+    }
+}
