d73057a3090a7cc92ee04b7b0f057ea461b81a81
Benedikt Schmidt <benediktibk@aon.at>
2014-07-24 17:28:36 +0200

    implemented binding of lines

--- source/Database/Line.cs
+++ source/Database/Line.cs
@@ -1,7 +1,26 @@
-﻿namespace Database
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Database
 {
-    public class Line
+    public class Line : INotifyPropertyChanged
     {
+        #region variables
+
+        private string _name;
+        private Node _nodeOne;
+        private Node _nodeTwo;
+        private double _seriesResistancePerUnitLength;
+        private double _seriesInductancePerUnitLength;
+        private double _shuntConductancePerUnitLength;
+        private double _shuntCapacityPerUnitLength;
+        private double _length;
+
+        #endregion
+
+        #region constructor
+
         public Line()
         {
             SeriesResistancePerUnitLength = 1;
@@ -11,12 +30,120 @@
             ShuntConductancePerUnitLength = 0;
         }
 
-        public Node NodeOne { get; set; }
-        public Node NodeTwo { get; set; }
-        public double SeriesResistancePerUnitLength { get; set; }
-        public double SeriesInductancePerUnitLength { get; set; }
-        public double ShuntConductancePerUnitLength { get; set; }
-        public double ShuntCapacityPerUnitLength { get; set; }
-        public double Length { get; set; }
+        #endregion
+
+        #region properties
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name == value) return;
+
+                _name = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public Node NodeOne
+        {
+            get { return _nodeOne; }
+            set
+            {
+                if (_nodeOne == value) return;
+
+                _nodeOne = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public Node NodeTwo
+        {
+            get { return _nodeTwo; }
+            set
+            {
+                if (_nodeTwo == value) return;
+
+                _nodeTwo = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public double SeriesResistancePerUnitLength
+        {
+            get { return _seriesResistancePerUnitLength; }
+            set
+            {
+                if (_seriesResistancePerUnitLength == value) return;
+
+                _seriesResistancePerUnitLength = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public double SeriesInductancePerUnitLength
+        {
+            get { return _seriesInductancePerUnitLength; }
+            set
+            {
+                if (_seriesInductancePerUnitLength == value) return;
+
+                _seriesInductancePerUnitLength = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public double ShuntConductancePerUnitLength
+        {
+            get { return _shuntConductancePerUnitLength; }
+            set
+            {
+                if (_shuntConductancePerUnitLength == value) return;
+
+                _shuntConductancePerUnitLength = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public double ShuntCapacityPerUnitLength
+        {
+            get { return _shuntCapacityPerUnitLength; }
+            set
+            {
+                if (_shuntCapacityPerUnitLength == value) return;
+
+                _shuntCapacityPerUnitLength = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public double Length
+        {
+            get { return _length; }
+            set
+            {
+                if (_length == value) return;
+
+                _length = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
     }
 }
