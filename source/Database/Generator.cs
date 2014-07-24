1f738953965e44ff51bf8cb4b4be553b069626c3
Benedikt Schmidt <benediktibk@aon.at>
2014-07-24 17:46:43 +0200

    implemented binding of generators

--- source/Database/Generator.cs
+++ source/Database/Generator.cs
@@ -1,15 +1,94 @@
-﻿namespace Database
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Database
 {
-    public class Generator
+    public class Generator: INotifyPropertyChanged
     {
+        #region variables
+
+        private string _name;
+        private Node _node;
+        private double _voltageMagnitude;
+        private double _realPower;
+
+        #endregion
+
+        #region constructor
+
         public Generator()
         {
             VoltageMagnitude = 1;
             RealPower = 0;
         }
 
-        public Node Node { get; set; }
-        public double VoltageMagnitude { get; set; }
-        public double RealPower { get; set; }
+        #endregion
+
+        #region properties
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name == value) return;
+
+                _name = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public Node Node
+        {
+            get { return _node; }
+            set
+            {
+                if (_node == value) return;
+
+                _node = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public double VoltageMagnitude
+        {
+            get { return _voltageMagnitude; }
+            set
+            {
+                if (_voltageMagnitude == value) return;
+
+                _voltageMagnitude = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public double RealPower
+        {
+            get { return _realPower; }
+            set
+            {
+                if (_realPower == value) return;
+
+                _realPower = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
     }
 }
