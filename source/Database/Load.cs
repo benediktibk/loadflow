4379889453a5754fcb11da1729711dd4e7d5b492
Benedikt Schmidt <benediktibk@aon.at>
2014-07-24 18:03:50 +0200

    implemented binding of loads

--- source/Database/Load.cs
+++ source/Database/Load.cs
@@ -1,15 +1,82 @@
-﻿using System.Numerics;
+﻿using System;
+using System.ComponentModel;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace Database
 {
-    public class Load
+    public class Load : INotifyPropertyChanged
     {
+        #region variables
+
+        private string _name;
+        private Node _node;
+        private Complex _value;
+
+        #endregion
+
+        #region constructor
+
         public Load()
         {
             Value = new Complex();
         }
 
-        public Node Node { get; set; }
-        public Complex Value { get; set; }
+        #endregion
+
+        #region properties
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name == value) return;
+
+                _name = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public Node Node
+        {
+            get { return _node; }
+            set
+            {
+                if (_node == value) return;
+
+                _node = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        public Complex Value
+        {
+            get { return _value; }
+            set
+            {
+                if (_value == value) return;
+
+                _value = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
     }
 }
