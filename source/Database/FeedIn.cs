d0cd10e961b728a2d1e70bd61bbd976e4f74d3c4
Benedikt Schmidt <benediktibk@aon.at>
2014-07-24 17:37:13 +0200

    implemented binding of feed-ins

--- source/Database/FeedIn.cs
+++ source/Database/FeedIn.cs
@@ -1,16 +1,94 @@
-﻿using System.Numerics;
+﻿using System;
+using System.ComponentModel;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace Database
 {
-    public class FeedIn
+    public class FeedIn : INotifyPropertyChanged
     {
+        #region variables
+
+        private string _name;
+        private Node _node;
+        private Complex _voltage;
+        private double _shortCircuitPower;
+
+        #endregion
+
+        #region constructor
+
         public FeedIn()
         {
             Voltage = new Complex(1, 0);
         }
 
-        public Node Node { get; set; }
-        public Complex Voltage { get; set; }
-        public double ShortCircuitPower { get; set; }
+        #endregion
+
+        #region properties
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name == value) return;
+
+                _name = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public Node Node
+        {
+            get { return _node; }
+            set
+            {
+                if (_node == value) return;
+
+                _node = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public Complex Voltage
+        {
+            get { return _voltage; }
+            set
+            {
+                if (_voltage == value) return;
+
+                _voltage = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public double ShortCircuitPower
+        {
+            get { return _shortCircuitPower; }
+            set
+            {
+                if (_shortCircuitPower == value) return;
+
+                _shortCircuitPower = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
     }
 }
