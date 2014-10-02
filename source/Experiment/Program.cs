eed58909bedf913d4d034b77ff52ec1fb0df51e1
Benedikt Schmidt <benediktibk@aon.at>
2014-10-02 14:53:04 +0200

    changed experiment to output some coefficients

--- source/Experiment/Program.cs
+++ source/Experiment/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Numerics;
 using Calculation.SinglePhase.SingleVoltageLevel;
 using Calculation.SinglePhase.SingleVoltageLevel.NodeVoltageCalculators;
@@ -10,100 +10,33 @@ namespace Experiment
     {
         static void Main(string[] args)
         {
-            const double targetPrecision = 0.00001;
-            const int iterations = 1000;
-            var calculators = new Dictionary<string, INodeVoltageCalculator>
-            {
-                {"node potential method", new NodePotentialMethod()},
-                {"curent iteration", new CurrentIteration(targetPrecision, iterations)},
-                {"newton raphson", new NewtonRaphsonMethod(targetPrecision, iterations)},
-                {"FDLF", new FastDecoupledLoadFlowMethod(targetPrecision, iterations)},
-                {
-                    "HELM 64 Bit",
-                    new HolomorphicEmbeddedLoadFlowMethod(targetPrecision, 50, new PrecisionLongDouble())
-                },
-                {
-                    "HELM with current iteration",
-                    new HolomorphicEmbeddedLoadFlowMethodWithIterativeMethod(targetPrecision,
-                        new CurrentIteration(targetPrecision, iterations))
-                },
-                {
-                    "HELM with newton raphson",
-                    new HolomorphicEmbeddedLoadFlowMethodWithIterativeMethod(targetPrecision,
-                        new NewtonRaphsonMethod(targetPrecision, iterations))
-                },
-                {
-                    "HELM 200 Bit",
-                    new HolomorphicEmbeddedLoadFlowMethod(targetPrecision/100, 80, new PrecisionMulti(200))
-                },
-                {
-                    "HELM 2000 Bit",
-                    new HolomorphicEmbeddedLoadFlowMethod(targetPrecision/100, 150, new PrecisionMulti(2000))
-                }
-            };
+            var powerNet = new PowerNet(2, 1);
+            powerNet.Admittances.AddConnection(0, 1, new Complex(1, 0));
+            var feedInNode = new Node() { Voltage = new Complex(1, 0) };
+            var loadNode = new Node() { Power = new Complex(-0.23, 0) };
+            powerNet.SetNode(0, feedInNode);
+            powerNet.SetNode(1, loadNode);
+            const int coefficientCount = 15;
+            var nodeVoltageCalculator = new HolomorphicEmbeddedLoadFlowMethod(0.0000001, coefficientCount,
+                new PrecisionLongDouble());
+            var calculator = new LoadFlowCalculator(nodeVoltageCalculator);
 
-            using (var file = new System.IO.StreamWriter("out.csv", false))
+            powerNet.CalculateMissingInformation(calculator);
+
+            var loadVoltage = powerNet.NodeVoltages[1].Real;
+            Console.WriteLine("load voltage: " + loadVoltage);
+            Console.WriteLine("error of load voltage: " + Math.Abs(1 - loadVoltage/0.641421356)*100 + "%");
+
+            using (var file = new StreamWriter(@"C:\Users\Public\coefficients.csv", false))
             {
-                foreach (var calculator in calculators)
+                for (var i = 0; i < coefficientCount; ++i)
                 {
-                    var border = DetermineBorderOfVoltageCollapse(calculator.Value, targetPrecision);
-                    var line = calculator.Key + ";" + border;
-                    Console.WriteLine(line);
-                    file.WriteLine(line);
+                    var coefficient = nodeVoltageCalculator.GetCoefficients(i)[0].Real;
+                    file.WriteLine(coefficient + ";");
                 }
             }
 
             Console.ReadKey();
-        }
-
-        private static double DetermineBorderOfVoltageCollapse(INodeVoltageCalculator nodeVoltageCalculator, double targetPrecision)
-        {
-            var lower = 0.0;
-            var upper = 0.25001;
-
-            while (upper - lower > 0.0001)
-                ReduceInterval(ref lower, ref upper, nodeVoltageCalculator,targetPrecision);
-
-            return (upper + lower)/2;
-        }
-
-        private static void ReduceInterval(ref double lower, ref double upper, INodeVoltageCalculator nodeVoltageCalculator, double targetPrecision)
-        {
-            var middle = (upper + lower)/2;
-
-            if (PreciseEnough(nodeVoltageCalculator, middle, targetPrecision))
-                lower = middle;
-            else
-                upper = middle;
-        }
-
-        private static bool PreciseEnough(INodeVoltageCalculator nodeVoltageCalculator, double load, double targetPrecision)
-        {
-            var powerNet = CreatePowerNet(load);
-            var calculator = new LoadFlowCalculator(nodeVoltageCalculator);
-            var voltageCollapse = powerNet.CalculateMissingInformation(calculator);
-
-            if (load > 0.25)
-                return voltageCollapse;
-
-            if (voltageCollapse)
-                return false;
-
-            var voltageShouldBe = 0.5 + Math.Sqrt(0.25 - load);
-            var voltageIs = powerNet.GetNodes()[1].Voltage;
-            var error = (voltageShouldBe - voltageIs).Magnitude / voltageShouldBe;
-            return error < targetPrecision;
-        }
-
-        private static PowerNet CreatePowerNet(double load)
-        {
-            var powerNet = new PowerNet(2, 1);
-            powerNet.Admittances.AddConnection(0, 1, new Complex(1, 0));
-            var feedInNode = new Node() {Voltage = new Complex(1, 0)};
-            var loadNode = new Node() {Power = new Complex((-1)*load, 0)};
-            powerNet.SetNode(0, feedInNode);
-            powerNet.SetNode(1, loadNode);
-            return powerNet;
         }
     }
 }
