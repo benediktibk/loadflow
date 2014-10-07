f19157a7d43eb01a4f3a2017519bc8a47663e460
Benedikt Schmidt <benediktibk@aon.at>
2014-10-07 17:52:19 +0200

    implemented feed in of sincal

---
+++ source/SincalConnector/FeedIn.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Data.OleDb;
+using System.IO;
+using System.Numerics;
+using DatabaseHelper;
+
+namespace SincalConnector
+{
+    public class FeedIn
+    {
+        #region constructor
+
+        public FeedIn(ISafeDatabaseRecord record, IReadOnlyDictionary<int, IReadOnlyNode> nodes)
+        {
+            Id = record.Parse<int>("Element_ID");
+            var admittanceType = record.Parse<int>("Flag_Typ");
+
+            if (admittanceType != 2)
+                throw new InvalidDataException("a feed-in must be specified by R/X and Sk");
+
+            ShortCircuitPower = record.Parse<double>("Sk2");
+            RealToImaginaryRatio = record.Parse<double>("R_X");
+            NodeId = record.Parse<int>("Node_ID");
+            C = record.Parse<double>("cact");
+            var voltageAngle = record.Parse<double>("delta");
+            var voltageType = record.Parse<int>("Flag_Lf");
+            double voltageMagnitude;
+
+            switch (voltageType)
+            {
+                case 3:
+                    var voltageMagnitudeRelative = record.Parse<double>("u");
+                    var nominalVoltage = nodes[NodeId].NominalVoltage;
+                    voltageMagnitude = voltageMagnitudeRelative*nominalVoltage;
+                    break;
+                case 6:
+                    voltageMagnitude = record.Parse<double>("Ug");
+                    break;
+                default:
+                    throw new InvalidDataException("only the voltage types '|uq| und delta' and '|Uq| und delta' are supported for a feed-in");
+            }
+
+            Voltage = Complex.FromPolarCoordinates(voltageMagnitude, voltageAngle);
+        }
+
+        #endregion
+
+        #region properties
+
+        public Complex Voltage { get; private set; }
+        public double ShortCircuitPower { get; private set; }
+        public double RealToImaginaryRatio { get; private set; }
+        public double C { get; private set; }
+        public int Id { get; private set; }
+        public int NodeId { get; private set; }
+
+        #endregion
+
+        #region static functions
+
+        public static OleDbCommand CreateCommandToFetchAll()
+        {
+            return new OleDbCommand("SELECT Element_ID,Flag_Typ,Sk2,R_X,Flag_Lf,delta,u,Ug,Node_ID,cact FROM Infeeder;");
+        }
+
+        #endregion
+    }
+}
--- source/SincalConnector/PowerNet.cs
+++ source/SincalConnector/PowerNet.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data.OleDb;
 using System.Linq;
 using DatabaseHelper;
@@ -12,6 +12,7 @@ namespace SincalConnector
 
         private OleDbConnection _databaseConnection;
         private readonly IList<Node> _nodes;
+        private readonly IList<FeedIn> _feedIns;
 
         #endregion
 
@@ -20,6 +21,7 @@ namespace SincalConnector
         public PowerNet(string database)
         {
             _nodes = new List<Node>();
+            _feedIns = new List<FeedIn>();
 
             var connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + database;
             _databaseConnection = new OleDbConnection(connectionString);
@@ -27,10 +29,22 @@ namespace SincalConnector
 
             var nodeFetchCommand = Node.CreateCommandToFetchAll();
             nodeFetchCommand.Connection = _databaseConnection;
+            var nodeByIds = new Dictionary<int, IReadOnlyNode>();
 
             using (var reader = new SafeDatabaseReader(nodeFetchCommand.ExecuteReader()))
                 while (reader.Next())
-                    _nodes.Add(new Node(reader, _databaseConnection));
+                {
+                    var node = new Node(reader, _databaseConnection);
+                    nodeByIds.Add(node.Id, node);
+                    _nodes.Add(node);
+                }
+
+            var feedInFetchCommand = FeedIn.CreateCommandToFetchAll();
+            feedInFetchCommand.Connection = _databaseConnection;
+
+            using (var reader = new SafeDatabaseReader(feedInFetchCommand.ExecuteReader()))
+                while (reader.Next())
+                    _feedIns.Add(new FeedIn(reader, nodeByIds));
         }
 
         #endregion
@@ -40,6 +54,11 @@ namespace SincalConnector
         public IReadOnlyList<IReadOnlyNode> Nodes
         {
             get { return _nodes.Cast<IReadOnlyNode>().ToList(); }
+        }
+
+        public IReadOnlyList<FeedIn> FeedIns
+        {
+            get { return new ReadOnlyCollection<FeedIn>(_feedIns); }
         }
 
         #endregion
