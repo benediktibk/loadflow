5aee6264e52eea996c517dfbac87161464c45358
Benedikt Schmidt <benediktibk@aon.at>
2014-10-14 11:52:09 +0200

    implemented check for not supported elements

--- source/SincalConnector/PowerNet.cs
+++ source/SincalConnector/PowerNet.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data.OleDb;
-using System.IO;
 using System.Linq;
 using DatabaseHelper;
 
@@ -12,8 +11,7 @@ namespace SincalConnector
     {
         #region variables
 
-        private OleDbConnection _databaseConnection;
-        private readonly double _frequency;
+        private double _frequency;
         private readonly IList<Terminal> _terminals; 
         private readonly IList<Node> _nodes;
         private readonly IList<FeedIn> _feedIns;
@@ -25,7 +23,32 @@ namespace SincalConnector
 
         #region constructor
 
-        public PowerNet(string database)
+        public PowerNet(string database) : this()
+        {
+            var connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + database;
+
+            using (var databaseConnection = new OleDbConnection(connectionString))
+            {
+                databaseConnection.Open();
+
+                DetermineFrequency(databaseConnection);
+                FetchNodes(databaseConnection);
+                FetchTerminals(databaseConnection);
+
+                var nodesByIds = CreateDictionaryNodeByIds();
+                var nodeIdsByElementIds = CreateDictionaryNodeIdsByElementIds();
+
+                FetchFeedIns(databaseConnection, nodesByIds, nodeIdsByElementIds);
+                FetchLoads(databaseConnection, nodeIdsByElementIds);
+                FetchTransmissionLines(databaseConnection, nodeIdsByElementIds);
+                FetchTransformers(databaseConnection, nodesByIds, nodeIdsByElementIds);
+
+                if (ContainsNotSupportedElement(databaseConnection))
+                    throw new NotSupportedException("the net contains a not supported element");
+            }
+        }
+
+        private PowerNet()
         {
             _terminals = new List<Terminal>();
             _nodes = new List<Node>();
@@ -33,75 +56,6 @@ namespace SincalConnector
             _loads = new List<Load>();
             _transmissionLines = new List<TransmissionLine>();
             _transformers = new List<Transformer>();
-            var nodesByIds = new Dictionary<int, IReadOnlyNode>();
-            var nodeIdsByElementIds = new MultiDictionary<int, int>();
-
-            var connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + database;
-            _databaseConnection = new OleDbConnection(connectionString);
-            _databaseConnection.Open();
-
-            var frequenciesFetchCommand = CreateCommandToFetchAllFrequencies();
-            frequenciesFetchCommand.Connection = _databaseConnection;
-            var frequencies = new List<double>();
-            
-            using (var reader = new SafeDatabaseReader(frequenciesFetchCommand.ExecuteReader()))
-                while (reader.Next())
-                    frequencies.Add(reader.Parse<double>("f"));
-
-            if (frequencies.Count != 1)
-                throw new NotSupportedException("only one frequency per net is supported");
-
-            _frequency = frequencies.First();
-
-            var nodeFetchCommand = Node.CreateCommandToFetchAll();
-            nodeFetchCommand.Connection = _databaseConnection;
-
-            using (var reader = new SafeDatabaseReader(nodeFetchCommand.ExecuteReader()))
-                while (reader.Next())
-                {
-                    var node = new Node(reader, _databaseConnection);
-                    nodesByIds.Add(node.Id, node);
-                    _nodes.Add(node);
-                }
-
-            var terminalFetchCommand = Terminal.CreateCommandToFetchAll();
-            terminalFetchCommand.Connection = _databaseConnection;
-
-            using (var reader = new SafeDatabaseReader(terminalFetchCommand.ExecuteReader()))
-                while (reader.Next())
-                {
-                    var terminal = new Terminal(reader);
-                    nodeIdsByElementIds.Add(terminal.ElementId, terminal.NodeId);
-                    _terminals.Add(terminal);
-                }
-
-            var feedInFetchCommand = FeedIn.CreateCommandToFetchAll();
-            feedInFetchCommand.Connection = _databaseConnection;
-
-            using (var reader = new SafeDatabaseReader(feedInFetchCommand.ExecuteReader()))
-                while (reader.Next())
-                    _feedIns.Add(new FeedIn(reader, nodesByIds, nodeIdsByElementIds));
-
-            var loadFetchCommand = Load.CreateCommandToFetchAll();
-            loadFetchCommand.Connection = _databaseConnection;
-
-            using (var reader = new SafeDatabaseReader(loadFetchCommand.ExecuteReader()))
-                while (reader.Next())
-                    _loads.Add(new Load(reader, nodeIdsByElementIds));
-
-            var transmissionLineFetchCommand = TransmissionLine.CreateCommandToFetchAll();
-            transmissionLineFetchCommand.Connection = _databaseConnection;
-
-            using (var reader = new SafeDatabaseReader(transmissionLineFetchCommand.ExecuteReader()))
-                while (reader.Next())
-                    _transmissionLines.Add(new TransmissionLine(reader, nodeIdsByElementIds, _frequency));
-
-            var transformerFetchCommand = Transformer.CreateCommandToFetchAll();
-            transformerFetchCommand.Connection = _databaseConnection;
-
-            using (var reader = new SafeDatabaseReader(transformerFetchCommand.ExecuteReader()))
-                while (reader.Next())
-                    _transformers.Add(new Transformer(reader, nodesByIds, nodeIdsByElementIds));
         }
 
         #endregion
@@ -144,7 +98,143 @@ namespace SincalConnector
 
         public static OleDbCommand CreateCommandToFetchAllFrequencies()
         {
-            return new OleDbCommand("SELECT f FROM VoltageLevel GROUP BY f");
+            return new OleDbCommand("SELECT f FROM VoltageLevel GROUP BY f;");
+        }
+
+        public static OleDbCommand CreateCommandToFetchAllElementIdsSorted()
+        {
+            return new OleDbCommand("SELECT Element_ID FROM Element ORDER BY Element_ID ASC;");
+        }
+
+        #endregion
+
+        #region private functions
+
+        private MultiDictionary<int, int> CreateDictionaryNodeIdsByElementIds()
+        {
+            var nodeIdsByElementIds = new MultiDictionary<int, int>();
+            foreach (var terminal in _terminals)
+                nodeIdsByElementIds.Add(terminal.ElementId, terminal.NodeId);
+            return nodeIdsByElementIds;
+        }
+
+        private Dictionary<int, IReadOnlyNode> CreateDictionaryNodeByIds()
+        {
+            var nodesByIds = new Dictionary<int, IReadOnlyNode>();
+            foreach (var node in _nodes)
+                nodesByIds.Add(node.Id, node);
+            return nodesByIds;
+        }
+
+        private void FetchTerminals(OleDbConnection databaseConnection)
+        {
+            var terminalFetchCommand = Terminal.CreateCommandToFetchAll();
+            terminalFetchCommand.Connection = databaseConnection;
+
+            using (var reader = new SafeDatabaseReader(terminalFetchCommand.ExecuteReader()))
+                while (reader.Next())
+                    _terminals.Add(new Terminal(reader));
+        }
+
+        private void FetchNodes(OleDbConnection databaseConnection)
+        {
+            var nodeFetchCommand = Node.CreateCommandToFetchAll();
+            nodeFetchCommand.Connection = databaseConnection;
+
+            using (var reader = new SafeDatabaseReader(nodeFetchCommand.ExecuteReader()))
+                while (reader.Next())
+                    _nodes.Add(new Node(reader, databaseConnection));
+        }
+
+        private void FetchTransformers(OleDbConnection databaseConnection, Dictionary<int, IReadOnlyNode> nodesByIds,
+            MultiDictionary<int, int> nodeIdsByElementIds)
+        {
+            var transformerFetchCommand = Transformer.CreateCommandToFetchAll();
+            transformerFetchCommand.Connection = databaseConnection;
+
+            using (var reader = new SafeDatabaseReader(transformerFetchCommand.ExecuteReader()))
+                while (reader.Next())
+                    _transformers.Add(new Transformer(reader, nodesByIds, nodeIdsByElementIds));
+        }
+
+        private void FetchTransmissionLines(OleDbConnection databaseConnection, MultiDictionary<int, int> nodeIdsByElementIds)
+        {
+            var transmissionLineFetchCommand = TransmissionLine.CreateCommandToFetchAll();
+            transmissionLineFetchCommand.Connection = databaseConnection;
+
+            using (var reader = new SafeDatabaseReader(transmissionLineFetchCommand.ExecuteReader()))
+                while (reader.Next())
+                    _transmissionLines.Add(new TransmissionLine(reader, nodeIdsByElementIds, _frequency));
+        }
+
+        private void FetchLoads(OleDbConnection databaseConnection, MultiDictionary<int, int> nodeIdsByElementIds)
+        {
+            var loadFetchCommand = Load.CreateCommandToFetchAll();
+            loadFetchCommand.Connection = databaseConnection;
+
+            using (var reader = new SafeDatabaseReader(loadFetchCommand.ExecuteReader()))
+                while (reader.Next())
+                    _loads.Add(new Load(reader, nodeIdsByElementIds));
+        }
+
+        private void FetchFeedIns(OleDbConnection databaseConnection, Dictionary<int, IReadOnlyNode> nodesByIds, MultiDictionary<int, int> nodeIdsByElementIds)
+        {
+            var feedInFetchCommand = FeedIn.CreateCommandToFetchAll();
+            feedInFetchCommand.Connection = databaseConnection;
+
+            using (var reader = new SafeDatabaseReader(feedInFetchCommand.ExecuteReader()))
+                while (reader.Next())
+                    _feedIns.Add(new FeedIn(reader, nodesByIds, nodeIdsByElementIds));
+        }
+
+        private void DetermineFrequency(OleDbConnection databaseConnection)
+        {
+            var frequenciesFetchCommand = CreateCommandToFetchAllFrequencies();
+            frequenciesFetchCommand.Connection = databaseConnection;
+            var frequencies = new List<double>();
+
+            using (var reader = new SafeDatabaseReader(frequenciesFetchCommand.ExecuteReader()))
+                while (reader.Next())
+                    frequencies.Add(reader.Parse<double>("f"));
+
+            if (frequencies.Count != 1)
+                throw new NotSupportedException("only one frequency per net is supported");
+
+            _frequency = frequencies.First();
+        }
+
+        private bool ContainsNotSupportedElement(OleDbConnection databaseConnection)
+        {
+            var elementFetchCommand = CreateCommandToFetchAllElementIdsSorted();
+            elementFetchCommand.Connection = databaseConnection;
+            var allSupportedElements = GetAllSupportedElementIdsSorted();
+            var allElements = new List<int>();
+
+            using (var reader = new SafeDatabaseReader(elementFetchCommand.ExecuteReader()))
+                while (reader.Next())
+                    allElements.Add(reader.Parse<int>("Element_ID"));
+
+            if (allSupportedElements.Count != allElements.Count)
+                return true;
+
+            return allElements.Where((t, i) => t != allSupportedElements[i]).Any();
+        }
+
+        private List<int> GetAllSupportedElementIdsSorted()
+        {
+            var result = new List<int>(_feedIns.Count + _loads.Count + _transformers.Count + _transmissionLines.Count);
+            var originalCapacity = result.Capacity;
+
+            result.AddRange(_feedIns.Select(element => element.Id));
+            result.AddRange(_loads.Select(element => element.Id));
+            result.AddRange(_transformers.Select(element => element.Id));
+            result.AddRange(_transmissionLines.Select(element => element.Id));
+
+            if (originalCapacity != result.Capacity)
+                throw new Exception("not enough space was allocated for the element IDs");
+
+            result.Sort();
+            return result;
         }
 
         #endregion
