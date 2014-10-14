b9d720ce230bebb921bf53d7002fb6dadd392a00
Benedikt Schmidt <benediktibk@aon.at>
2014-10-14 16:01:15 +0200

    added properties to impedance load

--- source/SincalConnector/ImpedanceLoad.cs
+++ source/SincalConnector/ImpedanceLoad.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.OleDb;
+using System.Numerics;
+using DatabaseHelper;
 
 namespace SincalConnector
 {
@@ -10,7 +8,7 @@ namespace SincalConnector
     {
         #region constructor
 
-        public ImpedanceLoad()
+        public ImpedanceLoad(ISafeDatabaseRecord record, IReadOnlyMultiDictionary<int, int> nodeIdsByElementIds)
         {
             
         }
@@ -19,9 +17,18 @@ namespace SincalConnector
 
         #region properties
 
+        public int Id { get; private set; }
+        public int NodeId { get; private set; }
+        public Complex Impedance { get; private set; }
+
         #endregion
 
         #region static functions
+
+        public static OleDbCommand CreateCommandToFetchAll()
+        {
+            return new OleDbCommand("SELECT Element_ID,Flag_Lf,P,Q FROM Load WHERE Flag_LoadType = 1 AND Flag_Load = 1;");
+        }
 
         #endregion
     }
