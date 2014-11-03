a31c6159c25e23c41f1e94812b69a6f617d45ea0
Benedikt Schmidt <benediktibk@aon.at>
2014-11-03 19:47:21 +0100

    added tests for SymmetricPowerNet

--- source/Core/CalculationTest/ThreePhase/SymmetricPowerNetTest.cs
+++ source/Core/CalculationTest/ThreePhase/SymmetricPowerNetTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 using Calculation.SinglePhase.MultipleVoltageLevels;
 using Calculation.ThreePhase;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -55,6 +56,52 @@ namespace CalculationTest.ThreePhase
             _powerNet.AddGenerator(3, 4, 5);
 
             _singlePhasePowerNetMock.Verify(x => x.AddGenerator(3, 4/Math.Sqrt(3), 5.0/3),
+                Times.Once);
+        }
+
+        [TestMethod]
+        public void AddFeedIn_ValidData_MockPowerNetGotCallToAddCorrectFeedIn()
+        {
+            _powerNet.AddFeedIn(3, new Complex(4, 5), 6, 7, 8);
+
+            _singlePhasePowerNetMock.Verify(x => x.AddFeedIn(3, new Complex(4, 5) / Math.Sqrt(3), 6.0/3, 7, 8),
+                Times.Once);
+        }
+
+        [TestMethod]
+        public void AddTwoWindingTransformer_ValidData_MockPowerNetGotCallToAddCorrectTwoWindingTransformer()
+        {
+            _powerNet.AddTwoWindingTransformer(3, 4, 5, 6, 7, 8, 9, 10, new Angle(2), "asdf");
+
+            _singlePhasePowerNetMock.Verify(x => x.AddTwoWindingTransformer(3, 4, 5.0/3, 6, 7.0/3, 8.0/3, 9, 10, It.IsAny<Angle>(), "asdf"),
+                Times.Once);
+        }
+
+        [TestMethod]
+        public void AddThreeWindingTransformer_ValidData_MockPowerNetGotCallToAddCorrectThreeWindingTransformer()
+        {
+            _powerNet.AddThreeWindingTransformer(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, new Angle(),
+                new Angle(), new Angle(), "asdf");
+
+            _singlePhasePowerNetMock.Verify(x => x.AddThreeWindingTransformer(3, 4, 5, 6.0 / 3, 7.0 / 3, 8.0 / 3, 9, 10, 11, 12.0 / 3, 13.0 / 3, 14.0 / 3, 15.0 / 3, 16, It.IsAny<Angle>(), It.IsAny<Angle>(), It.IsAny<Angle>(), "asdf"),
+                Times.Once);
+        }
+
+        [TestMethod]
+        public void AddLoad_ValidData_MockPowerNetGotCallToAddCorrectLoad()
+        {
+            _powerNet.AddLoad(3, new Complex(4, 5));
+
+            _singlePhasePowerNetMock.Verify(x => x.AddLoad(3, new Complex(4, 5) / 3),
+                Times.Once);
+        }
+
+        [TestMethod]
+        public void AddImpedanceLoad_ValidData_MockPowerNetGotCallToAddCorrectImpedanceLoad()
+        {
+            _powerNet.AddImpedanceLoad(3, new Complex(4, 5));
+
+            _singlePhasePowerNetMock.Verify(x => x.AddImpedanceLoad(3, new Complex(4, 5)),
                 Times.Once);
         }
     }
