5cc2631ab8b6846f8de10f724334dcb0fbc5566b
Benedikt Schmidt <benediktibk@aon.at>
2014-11-03 19:19:05 +0100

    added test for SymmetricPowerNet

--- source/Core/CalculationTest/ThreePhase/SymmetricPowerNetTest.cs
+++ source/Core/CalculationTest/ThreePhase/SymmetricPowerNetTest.cs
@@ -1,9 +1,33 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Calculation.SinglePhase.MultipleVoltageLevels;
+using Calculation.ThreePhase;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Misc;
+using Moq;
 
 namespace CalculationTest.ThreePhase
 {
     [TestClass]
     public class SymmetricPowerNetTest
     {
+        private SymmetricPowerNet _powerNet;
+        private Mock<IPowerNetComputable> _singlePhasePowerNetMock;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _singlePhasePowerNetMock = new Mock<IPowerNetComputable>();
+            _powerNet = new SymmetricPowerNet(_singlePhasePowerNetMock.Object);
+        }
+
+        [TestMethod]
+        public void SlackPhaseShift_MockPowerNet_SlackPhaseShiftOfMockPowerNet()
+        {
+            var resultShouldBe = new Angle(2);
+            _singlePhasePowerNetMock.Setup(x => x.SlackPhaseShift).Returns(resultShouldBe);
+
+            var result = _powerNet.SlackPhaseShift;
+
+            Assert.IsTrue(Angle.Equal(resultShouldBe, result, 0.000001));
+        }
     }
 }
