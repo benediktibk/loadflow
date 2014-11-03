baa5fbd698e0ae08c04d1c07ff385182b92d2621
Benedikt Schmidt <benediktibk@aon.at>
2014-11-03 19:27:22 +0100

    added tests for SymmetricPowerNet

--- source/Core/CalculationTest/ThreePhase/SymmetricPowerNetTest.cs
+++ source/Core/CalculationTest/ThreePhase/SymmetricPowerNetTest.cs
@@ -1,4 +1,5 @@
-﻿using Calculation.SinglePhase.MultipleVoltageLevels;
+﻿using System;
+using Calculation.SinglePhase.MultipleVoltageLevels;
 using Calculation.ThreePhase;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Misc;
@@ -28,6 +29,33 @@ namespace CalculationTest.ThreePhase
             var result = _powerNet.SlackPhaseShift;
 
             Assert.IsTrue(Angle.Equal(resultShouldBe, result, 0.000001));
+        }
+
+        [TestMethod]
+        public void AddNode_ValidData_MockPowerNetGotCallToAddCorrectNode()
+        {
+            _powerNet.AddNode(3, 5, "asdf");
+
+            _singlePhasePowerNetMock.Verify(x => x.AddNode(3, 5/Math.Sqrt(3), 0, "asdf"),
+                Times.Once);
+        }
+
+        [TestMethod]
+        public void AddTransmissionLine_ValidData_MockPowerNetGotCallToAddCorrectTransmissionLine()
+        {
+            _powerNet.AddTransmissionLine(3, 4, 5, 6, 7, 8, 9, false);
+
+            _singlePhasePowerNetMock.Verify(x => x.AddTransmissionLine(3, 4, 5, 6, 7, 8, 9, false),
+                Times.Once);
+        }
+
+        [TestMethod]
+        public void AddGenerator_ValidData_MockPowerNetGotCallToAddCorrectGenerator()
+        {
+            _powerNet.AddGenerator(3, 4, 5);
+
+            _singlePhasePowerNetMock.Verify(x => x.AddGenerator(3, 4/Math.Sqrt(3), 5.0/3),
+                Times.Once);
         }
     }
 }
