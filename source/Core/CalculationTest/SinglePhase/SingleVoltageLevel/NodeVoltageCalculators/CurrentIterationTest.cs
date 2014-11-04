12e2cdb5c5667bcc6eb773b2e6cdb1a39f2cad26
Benedikt Schmidt <benediktibk@aon.at>
2014-11-04 17:05:35 +0100

    added tests for CurrentIteration

---
+++ source/Core/CalculationTest/SinglePhase/SingleVoltageLevel/NodeVoltageCalculators/CurrentIterationTest.cs
@@ -0,0 +1,30 @@
+﻿using Calculation.SinglePhase.SingleVoltageLevel.NodeVoltageCalculators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculationTest.SinglePhase.SingleVoltageLevel.NodeVoltageCalculators
+{
+    [TestClass]
+    public class CurrentIterationTest : NodeVoltageCalculatorTest
+    {
+
+        public override double PrecisionPqOnly
+        {
+            get { return 0.0001; }
+        }
+
+        public override double PrecisionPvOnly
+        {
+            get { return 0.05; }
+        }
+
+        public override double PrecisionPqAndPv
+        {
+            get { return 0.0001; }
+        }
+
+        public override INodeVoltageCalculator CreateNodeVoltageCalculator()
+        {
+            return new CurrentIteration(0.00000001, 1000);
+        }
+    }
+}
