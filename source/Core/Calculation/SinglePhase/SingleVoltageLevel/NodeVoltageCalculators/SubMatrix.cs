c223becdf104453fcd963ee08cf54763266bb967
Benedikt Schmidt <benediktibk@aon.at>
2014-12-03 12:10:12 +0100

    implementes SubMatrix

---
+++ source/Core/Calculation/SinglePhase/SingleVoltageLevel/NodeVoltageCalculators/SubMatrix.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace Calculation.SinglePhase.SingleVoltageLevel.NodeVoltageCalculators
+{
+    public class SubMatrix
+    {
+        private readonly Matrix<double> _matrix;
+        private readonly int _rowOffset;
+        private readonly int _columnOffset;
+        private readonly int _rows;
+        private readonly int _columns;
+
+        public SubMatrix(Matrix<double> matrix, int rowOffset, int columnOffset, int rows, int columns)
+        {
+            if (matrix == null)
+                throw new ArgumentNullException("matrix");
+
+            if (rowOffset < 0)
+                throw new ArgumentOutOfRangeException("rowOffset", "must be positive");
+
+            if (columnOffset < 0)
+                throw new ArgumentOutOfRangeException("columnOffset", "must be positive");
+
+            if (rows < 0)
+                throw new ArgumentOutOfRangeException("rows", "must be positive");
+
+            if (columns < 0)
+                throw new ArgumentOutOfRangeException("columns", "must be positive");
+
+            if (rowOffset + rows > matrix.RowCount)
+                throw new ArgumentOutOfRangeException("rows", "too many rows selected");
+
+            if (columnOffset + columns > matrix.ColumnCount)
+                throw new ArgumentOutOfRangeException("columns", "too many columns selected");
+
+            _matrix = matrix;
+            _rowOffset = rowOffset;
+            _columnOffset = columnOffset;
+            _rows = rows;
+            _columns = columns;
+        }
+
+        public double this[int row, int column]
+        {
+            get 
+            {
+                CheckRange(row, column);
+                return _matrix[row + _rowOffset, column + _columnOffset];
+            }
+
+            set
+            {
+                CheckRange(row, column);
+                _matrix[row + _rowOffset, column + _columnOffset] = value;
+            }
+        }
+
+        private void CheckRange(int row, int column)
+        {
+            if (row >= _rows)
+                throw new ArgumentOutOfRangeException("row");
+
+            if (column >= _columns)
+                throw new ArgumentOutOfRangeException("column");
+        }
+    }
+}
---
+++ source/Core/CalculationTest/SinglePhase/SingleVoltageLevel/NodeVoltageCalculators/SubMatrixTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Calculation.SinglePhase.SingleVoltageLevel.NodeVoltageCalculators;
+using MathNet.Numerics.LinearAlgebra.Double;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculationTest.SinglePhase.SingleVoltageLevel.NodeVoltageCalculators
+{
+    [TestClass]
+    public class SubMatrixTest
+    {
+        private DenseMatrix _matrix;
+        private SubMatrix _subMatrix;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _matrix = DenseMatrix.OfArray(new double[,]
+            {
+                {1, 2, 3, 4}, 
+                {5, 6, 7, 8}, 
+                {9, 10, 11, 12}
+            });
+            _subMatrix = new SubMatrix(_matrix, 0, 1, 2, 3);
+        }
+
+        [TestMethod]
+        public void Get_IndicesInRange_CorrectValue()
+        {
+            Assert.AreEqual(6, _subMatrix[1, 0], 0.000001);
+        }
+
+        [TestMethod]
+        public void Set_IndicesInRange_CorrectValueInMatrixSet()
+        {
+            _subMatrix[0, 1] = 100;
+
+            Assert.AreEqual(100, _matrix[0, 2], 0.000001);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Set_RowNotInRange_ExceptionThrown()
+        {
+            _subMatrix[2, 1] = 100;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Set_ColumnNotInRange_ExceptionThrown()
+        {
+            _subMatrix[0, 3] = 100;
+        }
+    }
+}
