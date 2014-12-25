a5348421cc61d3c165f1919fd360d97c4199369b
Benedikt Schmidt <benediktibk@aon.at>
2014-12-25 23:37:42 +0100

    fixed parameter of BiCGSTAB

--- source/Core/HELM/Calculator.cpp
+++ source/Core/HELM/Calculator.cpp
@@ -81,7 +81,7 @@ void Calculator<Floating, ComplexFloating>::calculate()
 {          
 	freeMemory();
 	_coefficientStorage = new CoefficientStorage<ComplexFloating, Floating>(_numberOfCoefficients, _nodeCount, _pqBuses, _pvBuses, _admittances);
-	_solver = new LinearEquationSystemSolver<ComplexFloating, Floating>(_admittances, Floating(1e-10));
+	_solver = new LinearEquationSystemSolver<ComplexFloating, Floating>(_admittances, Floating(_targetPrecision*1e-3));
 	{
 		lock_guard<mutex> lock(_progressMutex);
 		_progress = 0;
