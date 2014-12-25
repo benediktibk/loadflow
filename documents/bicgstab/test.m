5633d307f6d4b98bedd79316375858eea1e221ec
Benedikt Schmidt <benediktibk@aon.at>
2014-12-25 22:40:15 +0100

    added matlab implementation of BiCGSTAB

---
+++ documents/bicgstab/test.m
@@ -0,0 +1,21 @@
+A = [   1+2i,   3+4i,   5+6i;
+        0       7+8i,   9+10i;
+        11+12i, 0,      13+14i];
+x = [15+16i; 17+18i; 19+20i];
+b = A*x;
+P = diag(diag(A));
+x0 = zeros(3, 1);
+maximumIterations = 10;
+epsilon = 1e-10;
+
+xMatlab = bicgstab(A, b, epsilon, maximumIterations, P);
+matlabError = norm(xMatlab - x);
+xOriginal = bicgstabOriginal(A, x0, b, P, maximumIterations, epsilon);
+originalError = norm(xOriginal - x);
+xModified = bicgstabModified(A, x0, b, P, maximumIterations, epsilon);
+modifiedError = norm(xModified - x);
+
+display(strcat('matlab error: ',num2str(matlabError)));
+display(strcat('original error: ',num2str(originalError)));
+display(strcat('modified error: ',num2str(modifiedError)));
+    
