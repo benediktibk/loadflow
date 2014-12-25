cf67458be046573a45399fdcd38fbd27570b0867
Benedikt Schmidt <benediktibk@aon.at>
2014-12-25 22:49:28 +0100

    simplified matlab implementation of BiCGSTAB

--- documents/bicgstab/bicgstabModified.m
+++ documents/bicgstab/bicgstabModified.m
@@ -2,41 +2,32 @@ function x = bicgstabModified(A, x, b, M, max_it, tol)
   r = b - A*x;
   omega = 1.0;
   alpha = 1.0;
-  r_tld = r;
+  rFirst = r;
 
-  for iter = 1:max_it,                              
+  for i = 1:max_it  
+     rho = (rFirst'*r);             
 
-     rho = (r_tld'*r);             
-
-     if ( iter > 1 ),
-        beta  = ( rho/rho_1 )*( alpha/omega );
-        p = r + beta*( p - omega*v );
+     if ( i > 1 ),
+        beta  = (rho/rhoPrevious)*(alpha/omega);
+        p = r + beta*(p - omega*v);
      else
         p = r;
      end
  
      p_hat = M \ p;
      v = A*p_hat;
-     alpha = rho / ( r_tld'*v );
+     alpha = rho/(rFirst'*v);
      s = r - alpha*v;
-     if ( norm(s) < tol ),                          
-        x = x + alpha*p_hat;
-        resid = norm(s);
-        break;
-     end
-
      s_hat = M \ s;                                 
      t = A*s_hat;
-     omega = ( t'*s) / ( t'*t );
-
-     x = x + alpha*p_hat + omega*s_hat;             
-
+     omega = (t'*s)/(t'*t);
+     x = x + alpha*p_hat + omega*s_hat;     
+     rhoPrevious = rho;
      r = s - omega*t;
+     
      error = norm(r);                     
-     if ( error <= tol ), break, end
-
-     if ( omega == 0.0 ), break, end
-     rho_1 = rho;
-
+     if (error <= tol)
+         break
+     end
   end
 end
