bad632301c89fe534abd5f6c875d4cd56ee3d39c
Benedikt Schmidt <benediktibk@aon.at>
2014-12-29 20:06:06 +0100

    removed files

--- source/Core/HELM/ComplexGreaterCompare.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-template<class Floating, class ComplexFloating>
-class ComplexGreaterCompare
-{
-public:
-	bool operator()(ComplexFloating const &a, ComplexFloating const &b) const;
-};
-
