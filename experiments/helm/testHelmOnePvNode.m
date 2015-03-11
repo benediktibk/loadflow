23200b260992127c483e356851fccc4bb4c0a710
Benedikt Schmidt <benediktibk@aon.at>
2015-03-11 10:12:40 +0100

    fixed test in experiment

--- experiments/helm/testHelmOnePvNode.m
+++ experiments/helm/testHelmOnePvNode.m
@@ -2,10 +2,10 @@ function [okay] = testHelmOnePvNode()
     admittances = [1 -1; -1 1];
     slackIndices = 1;
     slackVoltages = 1;
-    voltages = helm(admittances, slackIndices, slackVoltages, @rightHandSide, 50);
-    okay = norm(voltages - 0.641421356) < 1e-8;
+    voltages = helm(admittances, slackIndices, slackVoltages, @rightHandSide, 42);
+    okay = norm(voltages - 0.641421356) < 5e-2;
 end
 
 function [result] = rightHandSide(constantCurrents, rowAdmittanceSums, coefficients, inverseCoefficients, squaredCoefficients, admittanceWeightedCoefficients)
-    result = rightHandSidePv(constantCurrents(1), rowAdmittanceSums(1), coefficients(:, 1), squaredCoefficients(:, 1), admittanceWeightedCoefficients(:, 1), -0.23, 0.641421356);
+    result = rightHandSidePv(constantCurrents(1), rowAdmittanceSums(1) - 1, coefficients(:, 1), squaredCoefficients(:, 1), admittanceWeightedCoefficients(:, 1), -0.23, 0.641421356);
 end
