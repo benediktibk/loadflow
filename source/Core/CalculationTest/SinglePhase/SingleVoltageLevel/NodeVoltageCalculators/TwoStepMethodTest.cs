2d95494327123de7eaa9c6fe0bc09990038ee364
Benedikt Schmidt <benediktibk@outlook.com>
2020-09-22 17:17:30 +0200

    Remove local Path from Unit-Test Setup

--- source/Core/CalculationTest/SinglePhase/SingleVoltageLevel/NodeVoltageCalculators/TwoStepMethodTest.cs
+++ source/Core/CalculationTest/SinglePhase/SingleVoltageLevel/NodeVoltageCalculators/TwoStepMethodTest.cs
@@ -28,7 +28,7 @@ namespace CalculationTest.SinglePhase.SingleVoltageLevel.NodeVoltageCalculators
 
         public override INodeVoltageCalculator CreateNodeVoltageCalculator()
         {
-            return new TwoStepMethod(new HolomorphicEmbeddedLoadFlowMethod(@"D:\Users\benedikt.schmidt\Entwicklung\loadflow-git\source\Debug", 0.000001, 50, 64, true),
+            return new TwoStepMethod(new HolomorphicEmbeddedLoadFlowMethod(0.000001, 50, 64, true),
                 new CurrentIteration(0.000001, 1000, true));
         }
     }
